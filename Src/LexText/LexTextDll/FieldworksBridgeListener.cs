e3ccbe4a8f31ea4f23ba453b2a44091a17a414a6
Jason Naylor <jason_naylor@sil.org>
2012-02-13 17:22:53 +0100

    Missed file from revision 3209.

---
+++ Src/LexText/LexTextDll/FieldworksBridgeListener.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using SIL.FieldWorks.Common.FwUtils;
+using SIL.FieldWorks.FDO;
+using SIL.Utils;
+using XCore;
+
+namespace SIL.FieldWorks.XWorks.LexText
+{
+	class FieldworksBridgeListener : IxCoreColleague, IFWDisposable
+	{
+		private Mediator _mediator;
+		private const string SendReceive = @"send_receive";
+		private const string FLExBridgeName = @"FieldWorksBridge.exe";
+		private FdoCache Cache { get; set; }
+		#region IxCoreColleague Members
+
+		public IxCoreColleague[] GetMessageTargets()
+		{
+			return new[] {this};
+		}
+
+		public void Init(Mediator mediator, System.Xml.XmlNode configurationParameters)
+		{
+			CheckDisposed();
+
+			_mediator = mediator;
+			mediator.AddColleague(this);
+			Cache = (FdoCache)_mediator.PropertyTable.GetValue("cache");
+		}
+
+		public int Priority
+		{
+			get { return (int)ColleaguePriority.Low; }
+		}
+
+		public bool ShouldNotCall
+		{
+			get { return false; }
+		}
+
+		#endregion
+
+		public bool OnDisplayShowConflictReport(object parameters, ref UIItemDisplayProperties display)
+		{
+			var fullName = FullFieldWorksBridgePath();
+			display.Enabled = FileUtils.FileExists(fullName) && NotesFileIsPresent(Cache);
+			display.Visible = display.Enabled;
+			return true;
+		}
+
+		/// <summary>
+		/// Returns true if there is any Chorus Notes to view.
+		/// </summary>
+		/// <param name="cache"></param>
+		/// <returns></returns>
+		private static bool NotesFileIsPresent(FdoCache cache)
+		{
+			return cache.ProjectId.ProjectFolder != null;
+		}
+
+		public bool OnShowConflictReport(object commandObject)
+		{
+			LaunchFieldworksBridge(Path.Combine(Cache.ProjectId.ProjectFolder, Cache.ProjectId.Name + ".fwdata"),
+								   Environment.UserName,
+								   "view_notes");
+			return true;
+		}
+
+		public bool OnDisplayFLExBridge(object parameters, ref UIItemDisplayProperties display)
+		{
+			var fullName = FullFieldWorksBridgePath();
+			display.Enabled = FileUtils.FileExists(fullName);
+			display.Visible = display.Enabled;
+
+			return true; // We dealt with it.
+		}
+
+		private static string FullFieldWorksBridgePath()
+		{
+			return Path.Combine(DirectoryFinder.FWCodeDirectory, FLExBridgeName);
+		}
+
+		public bool OnFLExBridge(object commandObject)
+		{
+			LaunchFieldworksBridge(Path.Combine(Cache.ProjectId.ProjectFolder, Cache.ProjectId.Name + ".fwdata"),
+								   Environment.UserName,
+								   SendReceive);
+			return true;
+		}
+
+		private static void LaunchFieldworksBridge(string projectFolder, string userName, string command)
+		{
+			string args = "";
+			if(userName != null)
+			{
+				AddArg(ref args, "-u", userName);
+			}
+			if(projectFolder != null)
+			{
+				AddArg(ref args, "-p", projectFolder);
+			}
+			AddArg(ref args, "-v", command);
+			Process.Start(FullFieldWorksBridgePath(), args);
+		}
+
+		private static void AddArg(ref string extant, string flag, string value)
+		{
+			if (!string.IsNullOrEmpty(extant))
+			{
+				extant += " ";
+			}
+			extant += flag;
+			if (!string.IsNullOrEmpty(value))
+			{
+				bool hasWhitespace;
+				if (value.Any(Char.IsWhiteSpace))
+				{
+					extant += " \"" + value + "\"";
+				}
+				else
+				{
+					extant += " " + value;
+				}
+			}
+		}
+
+		#region Implementation of IDisposable
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		/// <filterpriority>2</filterpriority>
+		public void Dispose()
+		{
+			_mediator.RemoveColleague(this);
+			IsDisposed = true;
+		}
+
+		#endregion
+
+		#region Implementation of IFWDisposable
+
+		/// <summary>
+		/// This method throws an ObjectDisposedException if IsDisposed returns
+		///              true.  This is the case where a method or property in an object is being
+		///              used but the object itself is no longer valid.
+		///              This method should be added to all public properties and methods of this
+		///              object and all other objects derived from it (extensive).
+		/// </summary>
+		public void CheckDisposed()
+		{
+			if (IsDisposed)
+				throw new ObjectDisposedException("FieldworksBridgeListener already disposed.");
+		}
+
+		/// <summary>
+		/// Add the public property for knowing if the object has been disposed of yet
+		/// </summary>
+		public bool IsDisposed { get; set; }
+
+		#endregion
+	}
+}
