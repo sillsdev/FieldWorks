3857e65c8cb608fa8b6f2d4a08c799f1cd247500
Eberhard Beilharz <eb1@sil.org>
2012-03-20 11:53:01 +0100

    Run xWorksTests with MTA on Mono (FWNX-837)
    When we try to run the xWorksTests with STA on Mono they keep
    hanging. Running with MTA solves this problem (besides the other
    fixes in other changelists).
    
    We can't have a preprocessor define in the App.config file, so we
    add AssemblyInfo.cs that sets STA on Windows only.
    
    Change-Id: Id041e8a21f498f4dabe3d8be6f519b85cdfba801
    Reviewed-on: http://hydra.wycliffe.ca:9160/589
    Reviewed-by: Stephen McConnel <stephen_mcconnel@sil.org>
    Tested-by: Jenkins <jenkins@sil.org>

---
+++ Src/xWorks/xWorksTests/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------------------------------
+// <copyright from='2012' to='2012' company='SIL International'>
+// 	Copyright (c) 2012, SIL International. All Rights Reserved.
+//
+// 	Distributable under the terms of either the Common Public License or the
+// 	GNU Lesser General Public License, as specified in the LICENSING.txt file.
+// </copyright>
+// --------------------------------------------------------------------------------------------
+
+// On Windows we need STA because of the COM objects. On Linux the tests hang when we use
+// STA. Since we don't have a "real" COM implementation we don't really need it on Linux.
+#if !__MonoCS__
+	[assembly: RequiresSTA]
+#endif
