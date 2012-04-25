2f3faa5f5f278c10ad8594494fefdb8eeaff637b
Eric Pyle <eric_pyle@sil.org>
2012-04-25 21:33:28 +0100

    LT-7581. (7000058) Data migration to fill GlossAppend fields     for the standard system Plural and Past variant types.

---
+++ Src/FDO/DomainServices/DataMigration/DataMigration7000058.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Xml.XPath;
+
+namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
+{
+	/// ----------------------------------------------------------------------------------------
+	/// <summary>
+	/// Migrate data from 7000057 to 7000058.
+	///
+	/// Data migration to change Irregularly Inflected Form variant types to class LexEntryInflType (for LT-7581).
+	/// </summary>
+	/// <remarks>
+	/// Actually, this DM will try to delete the old one and
+	/// </remarks>
+	/// ----------------------------------------------------------------------------------------
+	internal class DataMigration7000058 : IDataMigration
+	{
+		#region IDataMigration Members
+
+		public void PerformMigration(IDomainObjectDTORepository domainObjectDtoRepository)
+		{
+			DataMigrationServices.CheckVersionNumber(domainObjectDtoRepository, 7000057);
+			{
+				var dtoVariantPluralType = domainObjectDtoRepository.GetDTO(LexEntryTypeTags.kguidLexTypPluralVar.ToString());
+				AddGlossAppendIfEmpty(domainObjectDtoRepository, dtoVariantPluralType, ".pl");
+			}
+			{
+				var dtoVariantPastType = domainObjectDtoRepository.GetDTO(LexEntryTypeTags.kguidLexTypPastVar.ToString());
+				AddGlossAppendIfEmpty(domainObjectDtoRepository, dtoVariantPastType, ".pst");
+			}
+			DataMigrationServices.IncrementVersionNumber(domainObjectDtoRepository);
+		}
+
+		static private void AddGlossAppendIfEmpty(IDomainObjectDTORepository dtoRepo, DomainObjectDTO dtoToChange, string glossAppend)
+		{
+			XElement dtoToChangeElt = XElement.Parse(dtoToChange.Xml);
+			XElement glossAppendElt = dtoToChangeElt.XPathSelectElement("GlossAppend");
+			if (glossAppendElt == null)
+			{
+				dtoToChangeElt.Add(XElement.Parse("<GlossAppend/>"));
+				glossAppendElt = dtoToChangeElt.XPathSelectElement("GlossAppend");
+			}
+			XElement aUniElt = glossAppendElt.XPathSelectElement("AUni[@ws='en']");
+			if (aUniElt == null)
+			{
+				glossAppendElt.Add(XElement.Parse("<AUni ws='en'/>"));
+				aUniElt = glossAppendElt.XPathSelectElement("AUni[@ws='en']");
+			}
+			if (aUniElt.Value.Trim().Length == 0)
+			{
+				aUniElt.Value = glossAppend;
+			}
+
+			DataMigrationServices.UpdateDTO(dtoRepo, dtoToChange, dtoToChangeElt.ToString());
+		}
+
+		#endregion
+	}
+}
