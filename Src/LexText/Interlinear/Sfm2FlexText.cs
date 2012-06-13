61c7142e66168c38389c3fcc27664cee9766afeb
Eric Pyle <eric_pyle@sil.org>
2012-06-13 11:06:20 +0100

    LT-8276. "Import SFM to Words and Glosses" finished stage 1, sfm to flextext fragment doc.

--- Src/LexText/Interlinear/Sfm2FlexText.cs
+++ Src/LexText/Interlinear/Sfm2FlexText.cs
@@ -8,12 +8,13 @@ using System.Windows.Forms;
 using System.Xml;
 using Sfm2Xml;
 using SIL.FieldWorks.Common.COMInterfaces;
+using SIL.FieldWorks.LexText.Controls;
 using SIL.FieldWorks.FDO.DomainServices;
 using SilEncConverters40;
 
 namespace SIL.FieldWorks.IText
 {
-	/// <summary>
+   /// <summary>
 	/// This class is responsible for converting SFM files to the FlexText interlinear XML format that we
 	/// know how to import.
 	/// The importer is designed to import a single text per operation. Therefore, we produce the output
@@ -22,99 +23,58 @@ namespace SIL.FieldWorks.IText
 	/// </summary>
 	[SuppressMessage("Gendarme.Rules.Design", "TypesWithDisposableFieldsShouldBeDisposableRule",
 		Justification="m_writer gets disposed in Convert method")]
-	internal class Sfm2FlexText
+	internal class Sfm2FlexText : Sfm2FlexTextBase<InterlinearMapping, InterlinDestination>
 	{
-		private Dictionary<string, InterlinearMapping> m_mappings = new Dictionary<string, InterlinearMapping>();
-		private XmlWriter m_writer;
-		private ILgWritingSystemFactory m_wsf;
-		private EncConverters m_encConverters;
-		private ITsStrFactory m_tsf;
-		private ByteReader m_reader;
 		// true when we have already added a "words" element to the current "phrase". Meaningless if there is not a phrase open at all.
 		private bool m_phraseHasWords;
 		private bool m_textHasContent;
-		private string m_pendingMarker; // marker pushed back because not the extra one we were looking for
-		private byte[] m_pendingData; // data corresponding to m_pendingMarker.
-		List<string> m_openElements = new List<string>();
-		List<string> m_docStructure = new List<string>(new [] { "document", "interlinear-text",
-			"paragraphs", "paragraph", "phrases", "phrase", "words", "word" });
 		// set emptied when we open a new phrase recording non-repeatable item-type/writing system combinations that have already occurred.
 		private HashSet<Tuple<InterlinDestination, string>> m_itemsInThisPhrase = new HashSet<Tuple<InterlinDestination, string>>();
 		// set emptied when we open a new text recording non-repeatable item-type/writing system combinations that have already occurred.
 		private HashSet<Tuple<InterlinDestination, string>> m_itemsInThisText = new HashSet<Tuple<InterlinDestination, string>>();
 
-		public byte[] Convert(ByteReader reader, List<InterlinearMapping> mappings, ILgWritingSystemFactory wsf)
+		internal Sfm2FlexText() : base (new List<string>(new [] { "document", "interlinear-text",
+			"paragraphs", "paragraph", "phrases", "phrase", "words", "word" }))
 		{
-			m_reader = reader;
-			m_wsf = wsf;
-			m_tsf = TsStrFactoryClass.Create();
-			using (var output = new MemoryStream())
+		}
+
+		protected override void WriteToDocElement(byte[] data, InterlinearMapping mapping)
+		{
+			switch (mapping.Destination)
 			{
-				using (m_writer = XmlWriter.Create(output, new XmlWriterSettings() { CloseOutput = true }))
-				{
-					WriteStartElement("document");
-					foreach (var mapping in mappings)
-						m_mappings[mapping.Marker] = mapping;
-					string marker;
-					byte[] data;
-					byte[] badData;
-					while (GetNextSfmMarkerAndData(out marker, out data))
-					{
-						InterlinearMapping mapping;
-						if (!m_mappings.TryGetValue(marker, out mapping))
-							continue; // ignore any markers we don't know.
-						switch (mapping.Destination)
-						{
-						// Todo: many cases need more checks for correct state.
-							default: // Ignored
-								break;
-							case InterlinDestination.Source:
-								MakeRootItem(mapping, data, "source");
-								break;
-							case InterlinDestination.Abbreviation:
-								MakeRootItem(mapping, data, "title-abbreviation");
-								break;
-							case InterlinDestination.Title:
-								MakeRootItem(mapping, data, "title");
-								break;
-							case InterlinDestination.Comment:
-								MakeRootItem(mapping, data, "comment");
-								break;
-							case InterlinDestination.ParagraphBreak:
-								HandleParaBreak();
-								break;
-							case InterlinDestination.Reference:
-								HandleReference(mapping, data);
-								break;
-							case InterlinDestination.Baseline:
-								HandleBaseline(mapping, data);
-								break;
-							case InterlinDestination.FreeTranslation:
-								MakeRepeatableItem(mapping, data, "gls", "phrase", m_itemsInThisPhrase);
-								break;
-							case InterlinDestination.LiteralTranslation:
-								MakeRepeatableItem(mapping, data, "lit", "phrase", m_itemsInThisPhrase);
-								break;
-							case InterlinDestination.Note:
-								MakeItem(mapping, data, "note", "phrase");
-								break;
-						}
-					}
-					m_writer.Close();
-				}
-				var result = output.ToArray();
-				if (Form.ActiveForm != null && (Form.ModifierKeys & Keys.Shift) == Keys.Shift)
-				{
-					// write out the intermediate file
-					var intermediatePath = Path.Combine(Path.GetDirectoryName(reader.FileName),
-					Path.GetFileNameWithoutExtension(reader.FileName) + "-intermediate.xml");
-					using (var stream = File.Create(intermediatePath))
-					{
-						stream.Write(result, 0, result.Length);
-						stream.Close();
-					}
-				}
-				return result;
+					// Todo: many cases need more checks for correct state.
+				default: // Ignored
+					break;
+				case InterlinDestination.Source:
+					MakeRootItem(mapping, data, "source");
+					break;
+				case InterlinDestination.Abbreviation:
+					MakeRootItem(mapping, data, "title-abbreviation");
+					break;
+				case InterlinDestination.Title:
+					MakeRootItem(mapping, data, "title");
+					break;
+				case InterlinDestination.Comment:
+					MakeRootItem(mapping, data, "comment");
+					break;
+				case InterlinDestination.ParagraphBreak:
+					HandleParaBreak();
+					break;
+				case InterlinDestination.Reference:
+					HandleReference(mapping, data);
+					break;
+				case InterlinDestination.Baseline:
+					HandleBaseline(mapping, data);
+					break;
+				case InterlinDestination.FreeTranslation:
+					MakeRepeatableItem(mapping, data, "gls", "phrase", m_itemsInThisPhrase);
+					break;
+				case InterlinDestination.LiteralTranslation:
+					MakeRepeatableItem(mapping, data, "lit", "phrase", m_itemsInThisPhrase);
+					break;
+				case InterlinDestination.Note:
+					MakeItem(mapping, data, "note", "phrase");
+					break;
 			}
 		}
 
@@ -124,74 +84,16 @@ namespace SIL.FieldWorks.IText
 		/// <param name="mapping"></param>
 		/// <param name="data"></param>
 		/// <param name="itemType"></param>
-		private void MakeRootItem(InterlinearMapping mapping, byte[] data, string itemType)
+		protected override void MakeRootItem(InterlinearMapping mapping, byte[] data, string itemType)
 		{
 			if (m_textHasContent)
 				WriteStartElementIn("interlinear-text", "document");
 			MakeRepeatableItem(mapping, data, itemType, "interlinear-text", m_itemsInThisText);
 		}
 
-		private bool GetNextSfmMarkerAndData(out string marker, out byte[] data)
+		protected override void WriteStartElement(string marker)
 		{
-			if (m_pendingMarker != null)
-			{
-				marker = m_pendingMarker;
-				data = m_pendingData;
-				m_pendingMarker = null;
-				return true;
-			}
-			return GetRawData(out marker, out data);
-		}
-
-		private bool GetRawData(out string marker, out byte[] data)
-		{
-			byte[] badData;
-			return m_reader.GetNextSfmMarkerAndData(out marker, out data, out badData);
-		}
-
-		/// <summary>
-		/// Read one more marker and data from the input. If it is the continuation marker, return true and the data;
-		/// otherwise save the data to be read later and return false.
-		/// </summary>
-		bool GetMoreData(string marker, out byte[] data)
-		{
-			string nextMarker;
-			if (!GetRawData(out nextMarker, out data))
-				return false;
-			if (nextMarker == marker)
-				return true;
-			m_pendingData = data;
-			m_pendingMarker = nextMarker;
-			data = null;
-			return false;
-		}
-
-
-		void WriteStartElementIn(string marker, string parentMarker)
-		{
-			AdjustDepth(parentMarker); //<-May initiate EndElement or StartElement calls as needed.
-			WriteStartElement(marker);
-		}
-
-		/// <summary>
-		/// Do whatever start or end element calls are needed so that we are at the level where the current
-		/// open element is parentMarker
-		/// <note>This implementation precludes any element name re-use in the output format,
-		/// i.e. a phrase element could not have a phrase element nested in it, or in any of its children</note>
-		/// </summary>
-		private void AdjustDepth(string parentMarker)
-		{
-			int depth = m_docStructure.IndexOf(parentMarker) + 1;
-			while (m_openElements.Count > depth)
-				WriteEndElement();
-			while (m_openElements.Count < depth)
-				WriteStartElement(m_docStructure[m_openElements.Count]);
-		}
-
-		void WriteStartElement(string marker)
-		{
-			m_writer.WriteStartElement(marker);
-			m_openElements.Add(marker);
+			base.WriteStartElement(marker);
 			if (marker == "phrase")
 			{
 				m_phraseHasWords = false;
@@ -206,12 +108,6 @@ namespace SIL.FieldWorks.IText
 				m_textHasContent = false;
 				m_itemsInThisText.Clear();
 			}
-		}
-
-		void WriteEndElement()
-		{
-			m_writer.WriteEndElement();
-			m_openElements.RemoveAt(m_openElements.Count -1);
 		}
 
 		private void HandleBaseline(InterlinearMapping mapping, byte[] data)
@@ -267,73 +163,6 @@ namespace SIL.FieldWorks.IText
 			WriteStartElementIn("word", "words");
 			MakeItem(mapping, text, itemType, "word");
 			WriteEndElement();
-		}
-
-		/// <summary>
-		/// Make an item of the type indicated by the mapping by first converting the given data using the mapping's converter.
-		/// It should have the specified item type and parent marker.
-		/// If there are following consecutive fields with the same marker combine them into a single item.
-		/// If a phrase is open and already has this type of element, ignore this one.
-		/// Set phraseHasElement to indicate that now it does.
-		/// </summary>
-		/// <param name="mapping"></param>
-		/// <param name="data"></param>
-		/// <param name="itemType"></param>
-		/// <param name="parentMarker"></param>
-		/// <param name="phraseHasElement"></param>
-		private void MakeRepeatableItem(InterlinearMapping mapping, byte[] data, string itemType, string parentMarker,
-			HashSet<Tuple<InterlinDestination, string>> itemsInThisParent)
-		{
-			var text = GetString(data, mapping).Trim();
-			byte[] moreData;
-			while (GetMoreData(mapping.Marker, out moreData))
-			{
-				var moreText = GetString(moreData, mapping).Trim();
-				if (string.IsNullOrEmpty(text))
-					text = moreText;
-				else
-					text = text + " " + moreText;
-			}
-			var key = new Tuple<InterlinDestination, string>(mapping.Destination, mapping.WritingSystem);
-			if (itemsInThisParent.Contains(key)
-				&& ParentElementIsOpen(parentMarker))
-			{
-				return;
-			}
-			itemsInThisParent.Add(key);
-			MakeItem(mapping, text, itemType, parentMarker);
-		}
-
-		// True if a phrase is open (some child element may also be open)
-		private bool ParentElementIsOpen(string parent)
-		{
-			return m_openElements.Count() > m_docStructure.IndexOf(parent);
-		}
-
-		private void MakeItem(InterlinearMapping mapping, byte[] data, string itemType, string parentMarker)
-		{
-			var text = GetString(data, mapping).Trim();
-			MakeItem(mapping, text, itemType, parentMarker);
-		}
-
-		private void MakeItem(InterlinearMapping mapping, string text, string itemType, string parentMarker)
-		{
-			WriteStartElementIn("item", parentMarker);
-			m_writer.WriteAttributeString("type", itemType);
-			if (!string.IsNullOrEmpty(mapping.WritingSystem))
-				m_writer.WriteAttributeString("lang", mapping.WritingSystem);
-			m_writer.WriteString(text);
-			WriteEndElement();
-		}
-
-		private string GetString(byte[] data, InterlinearMapping mapping)
-		{
-			if (string.IsNullOrEmpty(mapping.Converter))
-				return Encoding.UTF8.GetString(data); // todo: use encoding converter if present in mapping
-			if (m_encConverters == null)
-				m_encConverters = new EncConverters();
-			var converter = m_encConverters[mapping.Converter];
-			return converter.ConvertToUnicode(data);
 		}
 	}
 }
---
+++ Src/LexText/LexTextControls/LexTextControlsTests/WordsSfmImportTests.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using NUnit.Framework;
+using SIL.CoreImpl;
+using SIL.FieldWorks.FDO.DomainServices;
+using SIL.FieldWorks.LexText.Controls;
+using SIL.FieldWorks.Test.TestUtils;
+using Sfm2Xml;
+
+namespace LexTextControlsTests
+{
+	/// <summary>
+	/// These are largely adapted from the tests in SIL.FieldWorks.IText.InterlinSfmImportTests
+	/// </summary>
+	[TestFixture]
+	public class WordsSfmImportTests : BaseTest
+	{
+		[SuppressMessage("Gendarme.Rules.Portability", "NewLineLiteralRule",
+			Justification="New lines in input strings are different depending on platform")]
+		public WordsSfmImportTests()
+		{
+		}
+
+		/// <summary>
+		/// This tests out most aspects of the conversion.
+		/// </summary>
+		[Test]
+		public void BasicConversion()
+		{
+			const string input1 =
+@"\lx glossedonce
+\ge onlygloss
+\del 0
+\wc Pyle:PYLEE-1007
+\cdt 2012-06-04T08:06:14Z
+
+\lx glossedtwice
+\ge firstgloss
+\del 0
+\wc Pyle:PYLEE-1007
+\cdt 2012-06-05T08:06:14Z
+\ge secondgloss
+\del 1
+\wc Pyle:PYLEE-1007
+\cdt 2012-06-05T08:06:29Z
+\ddt 2012-06-07T08:49:08Z
+
+\lx support a phrase
+\ge phrase gloss
+\del 0
+\wc Pyle:PYLEE-1007
+\cdt 2012-06-05T08:23:54Z
+";
+			var mappings = new List<WordMapping>();
+			mappings.Add(new WordMapping { Marker = "lx", Destination = WordDestination.Wordform, WritingSystem = "qaa-x-kal" });
+			mappings.Add(new WordMapping { Marker = "ge", Destination = WordDestination.WordGloss, WritingSystem = "en" });
+			var wsf = GetWsf();
+			var input = new ByteReader("input1", Encoding.UTF8.GetBytes(input1));
+			var converter = new Sfm2FlexTextWordsFrag();
+			var output = converter.Convert(input, mappings, wsf);
+			using (var outputStream = new MemoryStream(output))
+			{
+				using (var reader = new StreamReader(outputStream))
+				{
+					var outputElt = XElement.Load(reader);
+					Assert.That(outputElt.Name.LocalName, Is.EqualTo("document"));
+					var words = outputElt.Elements("word").ToList();
+					Assert.That(words, Has.Count.EqualTo(3));
+
+					{
+						var word1 = words[0];
+						var txtItems = word1.XPathSelectElements("item[@type='txt']").ToList();
+						var glsItems = word1.XPathSelectElements("item[@type='gls']").ToList();
+						Assert.That(txtItems, Has.Count.EqualTo(1));
+						Assert.That(glsItems, Has.Count.EqualTo(1));
+						VerifyItem(word1, "./item[@type='txt']", "qaa-x-kal", "glossedonce");
+						VerifyItem(word1, "./item[@type='gls']", "en", "onlygloss");
+					}
+
+					{
+						var word2 = words[1];
+						var txtItems = word2.XPathSelectElements("item[@type='txt']").ToList();
+						var glsItems = word2.XPathSelectElements("item[@type='gls']").ToList();
+						Assert.That(txtItems, Has.Count.EqualTo(1));
+						Assert.That(glsItems, Has.Count.EqualTo(2));
+						VerifyItem(word2, "./item[@type='txt']", "qaa-x-kal", "glossedtwice");
+						VerifyItem(word2, "./item[@type='gls'][1]", "en", "firstgloss");
+						VerifyItem(word2, "./item[@type='gls'][2]", "en", "secondgloss");
+					}
+
+					{
+						var word3 = words[2];
+						var txtItems = word3.XPathSelectElements("item[@type='txt']").ToList();
+						var glsItems = word3.XPathSelectElements("item[@type='gls']").ToList();
+						Assert.That(txtItems, Has.Count.EqualTo(1));
+						Assert.That(glsItems, Has.Count.EqualTo(1));
+						VerifyItem(word3, "./item[@type='txt']", "qaa-x-kal", "support a phrase");
+						VerifyItem(word3, "./item[@type='gls']", "en", "phrase gloss");
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Hypothetically, an sfm file could just have a list of words without glosses.
+		/// create separate word elements for adjacent lx items
+		/// </summary>
+		[Test]
+		public void WordsWithoutGlosses()
+		{
+			const string input2 =
+@"\lx wordone
+\lx wordtwo
+\lx wordthree
+";
+			var mappings = new List<WordMapping>();
+			mappings.Add(new WordMapping {Marker = "lx", Destination = WordDestination.Wordform, WritingSystem = "qaa-x-kal"});
+			mappings.Add(new WordMapping {Marker = "ge", Destination = WordDestination.WordGloss, WritingSystem = "en"});
+			var wsf = GetWsf();
+			var input = new ByteReader("input2", Encoding.UTF8.GetBytes(input2));
+			var converter = new Sfm2FlexTextWordsFrag();
+			var output = converter.Convert(input, mappings, wsf);
+			using (var outputStream = new MemoryStream(output))
+			{
+				using (var reader = new StreamReader(outputStream))
+				{
+					var outputElt = XElement.Load(reader);
+					Assert.That(outputElt.Name.LocalName, Is.EqualTo("document"));
+					var words = outputElt.Elements("word").ToList();
+					Assert.That(words, Has.Count.EqualTo(3));
+					{
+						var word1 = words[0];
+						var txtItems = word1.XPathSelectElements("item[@type='txt']").ToList();
+						Assert.That(txtItems, Has.Count.EqualTo(1));
+						VerifyItem(word1, "./item[@type='txt']", "qaa-x-kal", "wordone");
+					}
+					{
+						var word2 = words[1];
+						var txtItems = word2.XPathSelectElements("item[@type='txt']").ToList();
+						Assert.That(txtItems, Has.Count.EqualTo(1));
+						VerifyItem(word2, "./item[@type='txt']", "qaa-x-kal", "wordtwo");
+					}
+					{
+						var word3 = words[2];
+						var txtItems = word3.XPathSelectElements("item[@type='txt']").ToList();
+						Assert.That(txtItems, Has.Count.EqualTo(1));
+						VerifyItem(word3, "./item[@type='txt']", "qaa-x-kal", "wordthree");
+					}
+
+				}
+			}
+		}
+
+		[Test]
+		[Ignore("Should we support gloss elements in sfm before their lx elements?")]
+		public void GlossesBeforeWords()
+		{
+		}
+
+		/// <summary>
+		/// NOTE: Copied from SIL.FieldWorks.IText.InterlinSfmImportTests
+		/// </summary>
+		/// <returns></returns>
+		private PalasoWritingSystemManager GetWsf()
+		{
+			var wsf = new PalasoWritingSystemManager();
+			IWritingSystem wsObj;
+			wsf.GetOrSet("qaa-x-kal", out wsObj);
+			EnsureQuoteAndHyphenWordForming(wsObj);
+			return wsf;
+		}
+
+		/// <summary>
+		/// NOTE: Copied from SIL.FieldWorks.IText.InterlinSfmImportTests
+		/// </summary>
+		/// <param name="wsObj"></param>
+		private void EnsureQuoteAndHyphenWordForming(IWritingSystem wsObj)
+		{
+			var validChars = ValidCharacters.Load(wsObj.ValidChars,
+				wsObj.DisplayLabel, null, null);
+			var fChangedSomething = false;
+			if (!validChars.IsWordForming('-'))
+			{
+				validChars.AddCharacter("-");
+				validChars.MoveBetweenWordFormingAndOther(new List<string>(new[] { "-" }), true);
+				fChangedSomething = true;
+			}
+			if (!validChars.IsWordForming('\''))
+			{
+				validChars.AddCharacter("'");
+				validChars.MoveBetweenWordFormingAndOther(new List<string>(new[] { "'" }), true);
+				fChangedSomething = true;
+			}
+			if (!fChangedSomething)
+				return;
+			wsObj.ValidChars = validChars.XmlString;
+		}
+
+		/// <summary>
+		/// VerifyItem(word, "./item[@type='" + itemType + "']", lang, text);
+		/// </summary>
+		/// <param name="textElt"></param>
+		/// <param name="xpath"></param>
+		/// <param name="expectedLang"></param>
+		/// <param name="expectedValue"></param>
+		private void VerifyItem(XElement textElt, string xpath, string expectedLang, string expectedValue)
+		{
+			var item = textElt.XPathSelectElements(xpath).FirstOrDefault();
+			Assert.That(item, Is.Not.Null);
+			Assert.That(item.Attribute("lang").Value, Is.EqualTo(expectedLang));
+			Assert.That(item.Value, Is.EqualTo(expectedValue));
+		}
+	}
+}
