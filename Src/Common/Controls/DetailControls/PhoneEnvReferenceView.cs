913c67b304b507176a44a9b3ed20737b2790a1b7
MarkS <marksvc@gmail.com>
2012-09-20 00:18:38 +0100

    Rename variable
    Change-Id: I8e6ea366b83c321e5eb259f248ae593f31b0bf1b
    Reviewed-on: http://hydra.wycliffe.ca:9160/746
    Reviewed-by: Eberhard Beilharz <eb1@sil.org>
    Tested-by: Jenkins <jenkins@sil.org>

--- Src/Common/Controls/DetailControls/PhoneEnvReferenceView.cs
+++ Src/Common/Controls/DetailControls/PhoneEnvReferenceView.cs
@@ -714,15 +714,15 @@ namespace SIL.FieldWorks.Common.Framework.DetailControls
 				var countOfExistingEnvironmentsInDatabaseForEntry = m_fdoCache.DomainDataByFlid.get_VecSize(m_rootObj.Hvo, m_rootFlid);
 				// Only reset the main property, if it has changed.
 				// Otherwise, the parser gets too excited about needing to reload.
-				int[] contents;
+				int[] existingListOfEnvironmentHvosInDatabaseForEntry;
 				int chvoMax = m_fdoCache.DomainDataByFlid.get_VecSize(m_rootObj.Hvo, m_rootFlid);
 				using (ArrayPtr arrayPtr = MarshalEx.ArrayToNative<int>(chvoMax))
 				{
 					m_fdoCache.DomainDataByFlid.VecProp(m_rootObj.Hvo, m_rootFlid, chvoMax, out chvoMax, arrayPtr);
-					contents = MarshalEx.NativeToArray<int>(arrayPtr, chvoMax);
+					existingListOfEnvironmentHvosInDatabaseForEntry = MarshalEx.NativeToArray<int>(arrayPtr, chvoMax);
 				}
 				if ((countOfExistingEnvironmentsInDatabaseForEntry != newHvos.Length)
-					|| !equalArrays(contents, newHvos))
+					|| !equalArrays(existingListOfEnvironmentHvosInDatabaseForEntry, newHvos))
 				{
 					m_fdoCache.DomainDataByFlid.Replace(m_rootObj.Hvo, m_rootFlid, 0, countOfExistingEnvironmentsInDatabaseForEntry, newHvos, newHvos.Length);
 				}
