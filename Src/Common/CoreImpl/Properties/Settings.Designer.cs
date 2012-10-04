dfdbbe724732302dd58b4cf136c080001101de95
Michael Lastufka <michael_lastufka@sil.org>
2012-10-04 17:12:44 +0100

    Changes needed to make the build work.

--- Src/Common/CoreImpl/Properties/Settings.Designer.cs
+++ Src/Common/CoreImpl/Properties/Settings.Designer.cs
@@ -10,19 +10,27 @@
 
 namespace SIL.CoreImpl.Properties {
 
-
+	/// <summary>
+	///
+	/// </summary>
 	[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
 	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
 	public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
 
 		private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
 
+		/// <summary>
+		///
+		/// </summary>
 		public static Settings Default {
 			get {
 				return defaultInstance;
 			}
 		}
 
+		/// <summary>
+		///
+		/// </summary>
 		[global::System.Configuration.UserScopedSettingAttribute()]
 		[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
 		[global::System.Configuration.DefaultSettingValueAttribute("True")]
