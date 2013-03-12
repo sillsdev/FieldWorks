01d48d5e46d163cf36b1494fd15d004cce13973b
Eberhard Beilharz <eb1@sil.org>
2013-03-12 18:14:12 +0100

    Cleanup obsolete files from Bld directory
    The Bld directory still contained several files from the obsolete
    NAnt build process. This change removes those files.
    
    Change-Id: If69c0194c56e7ad29e8a377ebf93234c1271e927

--- Bld/UseThis.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-if exist %1\UseThis.build.xml nant -f:%1\UseThis.build.xml -D:unregister=true doregisternet
-mkdir %1
-copy %0\..\*.* %1\
-nant -f:%1\UseThis.build.xml -D:targetdir=%1 -D:srcdir=%0\.. %2 %3 %4 %5 %6 %7 %8 %9 all
--- Bld/buildtest.bat
+++
@@ -1,4 +0,0 @@
-rem WILL ONLY WORK IF WE'RE ON THE RIGHT DRIVE
-REM HOW TO FIX THAT????
-cd %FWROOT%\Bld
-nant buildtest %1
