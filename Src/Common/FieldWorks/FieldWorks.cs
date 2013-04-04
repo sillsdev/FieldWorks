555957aead29163d6f730570afbc226eb4f10913
John Thomson <john_thomson@sil.org>
2013-04-04 10:34:55 -0500

    LT-14362, don't double-open project on create+SFM import
    Change-Id: I7da7b976dc5df788c3cc2c5becb1fafc56d9be67

--- Src/Common/FieldWorks/FieldWorks.cs
+++ Src/Common/FieldWorks/FieldWorks.cs
@@ -1567,6 +1567,7 @@ namespace SIL.FieldWorks
 							var projectLaunched = LaunchProject(args, ref projectToTry);
 							if(projectLaunched)
 							{
+								s_projectId = projectToTry; // Window is open on this project, we must not try to initialize it again.
 								var mainWindow = Form.ActiveForm;
 								if(mainWindow is IxWindow)
 								{
