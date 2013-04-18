6e5309158de3f932e4ae4fed00a9f11c33901dc2
Stephen McConnel <stephen_mcconnel@sil.org>
2013-04-18 10:22:26 -0500

    Fix for Linux FieldWorks to run okay on the develop branch
    Two relatively new items are preventing FieldWorks from running on
    Linux: the new hunspell code is not quite right for Linux, and the new
    message slice requires a chorus DLL which does not yet exist for Linux
    in the FieldWorks context.  (See FWNX-1056.)  This fixes the hunspell
    setup, and patches the message slice to not cause problems without a
    chorus DLL.
    (cherry picked from commit 691c37fc527af8f909e227e5cd1c47c965158ab7 on
    develop)
    
    Change-Id: I1ae4059b2ef731151bb7cffd6fb7f25491b79433

--- Src/LexText/Lexicon/MessageSlice.cs
+++ Src/LexText/Lexicon/MessageSlice.cs
@@ -36,6 +36,10 @@ namespace SIL.FieldWorks.XWorks.LexEd
 
 		public override void FinishInit()
 		{
+#if __MonoCS__
+			try
+			{
+#endif
 			m_chorusSystem = new ChorusSystem(Cache.ProjectId.ProjectFolder);
 			m_chorusSystem.InitWithoutHg(SendReceiveUser);
 			// This is a required object for CreateNotesBar. It specifies delegates for getting the information
@@ -49,6 +53,14 @@ namespace SIL.FieldWorks.XWorks.LexEd
 			m_notesBar = m_chorusSystem.WinForms.CreateNotesBar(dataFilePath, notesToRecordMapping, new NullProgress());
 			m_notesBar.SetTargetObject(m_obj);
 			this.Control = m_notesBar;
+#if __MonoCS__
+			}
+			catch
+			{
+				// This does not work yet on Linux, but shouldn't keep the rest of
+				// the lexicon edit tool from working!  (See FWNX-960.)
+			}
+#endif
 		}
 
 		// The notes bar expects to store notes about a particular file. Our notes are currently about the lexicon,
