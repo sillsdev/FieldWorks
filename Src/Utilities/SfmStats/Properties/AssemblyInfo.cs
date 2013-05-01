2c8205c27a9c201c4f11842e42330d3ae976e09b
Jason Naylor <jason_naylor@sil.org>
2013-05-01 13:28:39 -0500

    Prepare for successful patch building
    * Removed 2 obsolete dev testing projects (did not build anymore)
    * Added either CommonAssemblyInfo.cs or AssemblyInfoForTests.cs
      to .csproj files that didn't have them
    
    Change-Id: I92ad5c3abd4c5b9e90b986261611fa0deb8ca6a5

--- Src/Utilities/SfmStats/Properties/AssemblyInfo.cs
+++ Src/Utilities/SfmStats/Properties/AssemblyInfo.cs
@@ -7,13 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("sfmStats")]
 [assembly: AssemblyDescription("This program shows statistics on the passed in SFM file.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("sfmStats")]
-[assembly: AssemblyCopyright("Copyright © SIL International 2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,13 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4d4aa897-6ef3-4383-a8b5-1ac729b4558c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
