97f53f14f850229b6404d6ced7b3e4f86dcc6219
Jason Naylor <jason_naylor@sil.org>
2013-05-30 23:30:03 -0500

    Fix the HomographFixer to not throw exceptions
    * Modify test to catch bug with Citation Form and Allomorph
    * Modify the FixFwData exe to use a green screen crash box
    * Modify build to download Palaso artifacts instead of having them
      committed
    * Update the icu.net.dll to match the AssemblyVersion info for the
      latest Palaso.dll
    
    Change-Id: I514946960a2062dfe0146783633d485010c5f1d2

--- Src/Utilities/FixFwData/Program.cs
+++ Src/Utilities/FixFwData/Program.cs
@@ -9,13 +9,16 @@
 #endregion
 //
 // File: Program.cs
-// Responsibility: mcconnel
+// Responsibility: FLEx team
 // ---------------------------------------------------------------------------------------------
 using System;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows.Forms;
-using SIL.FieldWorks.FixData;
+using Palaso.Reporting;
 using SIL.FieldWorks.Common.FwUtils;
+using SIL.FieldWorks.FixData;
+using Palaso.UI.WindowsForms.HotSpot;
 
 namespace FixFwData
 {
@@ -25,22 +28,101 @@ namespace FixFwData
 			Justification = "Appears to be a bug in Gendarme...not recognizing that 0 and 1 are in correct range (0..255)")]
 		private static int Main(string[] args)
 		{
+			SetUpErrorHandling();
 			var pathname = args[0];
-			var prog = new ConsoleProgress();
-			var data = new FwDataFixer(pathname, prog, logger);
-			data.FixErrorsAndSave();
-			if (prog.DotsWritten)
-				Console.WriteLine();
+			using (var prog = new NullProgress())
+			{
+				var data = new FwDataFixer(pathname, prog, logger);
+				data.FixErrorsAndSave();
+			}
 			if (errorsOccurred)
 				return 1;
 			return 0;
 		}
 
 		private static bool errorsOccurred;
+
 		private static void logger(string guid, string date, string description)
 		{
 			Console.WriteLine(description);
 			errorsOccurred = true;
+		}
+
+		private static void SetUpErrorHandling()
+		{
+			using (new HotSpotProvider())
+			{
+				ErrorReport.EmailAddress = "flex_errors@sil.org";
+				ErrorReport.AddStandardProperties();
+				ExceptionHandler.Init();
+			}
+		}
+
+		private sealed class NullProgress : IProgress, IDisposable
+		{
+			public event CancelEventHandler Canceling;
+
+			public void Step(int amount)
+			{
+				if (Canceling != null)
+				{
+					// don't do anything -- this just shuts up the compiler about the
+					// event handler never being used.
+				}
+			}
+
+			public string Title { get; set; }
+
+			public string Message
+			{
+				get { return null; }
+				set { Console.Out.WriteLine(value); }
+			}
+
+			public int Position { get; set; }
+			public int StepSize { get; set; }
+			public int Minimum { get; set; }
+			public int Maximum { get; set; }
+			public Form Form { get; private set; }
+
+			public ProgressBarStyle ProgressBarStyle
+			{
+				get { return ProgressBarStyle.Continuous; }
+				set { }
+			}
+
+			public bool AllowCancel
+			{
+				get { return false; }
+				set { }
+			}
+			#region Gendarme required cruft
+#if DEBUG
+			/// <summary/>
+			~NullProgress()
+			{
+				Dispose(false);
+			}
+#endif
+
+			/// <summary/>
+			public void Dispose()
+			{
+				Dispose(true);
+				GC.SuppressFinalize(this);
+			}
+
+			/// <summary/>
+			private void Dispose(bool fDisposing)
+			{
+				System.Diagnostics.Debug.WriteLineIf(!fDisposing, "****** Missing Dispose() call for " + GetType() + ". *******");
+
+				if (Form != null && !Form.IsDisposed)
+				{
+					Form.Dispose();
+				}
+			}
+			#endregion
 		}
 	}
 }
