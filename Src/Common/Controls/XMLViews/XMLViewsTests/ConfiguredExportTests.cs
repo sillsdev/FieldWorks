91eb287c1926ff188517b07a793062e123ce1a6b
Jason Naylor <jason_naylor@sil.org>
2013-09-04 11:16:17 -0500

    Fix two problems with export and alphabetical headers
    * LT-13819 - the previous code to map secondary and
      tertiary sort rules to the main character was wrong
    * LT-14260 - there was no attempt to handle the mapping
      of digraphs and secondary and tertiary characters when
      the sort rules were 'simple'
    
    Change-Id: Ib9bc8e8a72398ac1a85062ae6f23a2278184e9ed

--- Src/Common/Controls/XMLViews/XMLViewsTests/ConfiguredExportTests.cs
+++ Src/Common/Controls/XMLViews/XMLViewsTests/ConfiguredExportTests.cs
@@ -1,23 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Xml;
 using NUnit.Framework;
+using Palaso.WritingSystems;
 using SIL.FieldWorks.Common.Controls;
 using SIL.FieldWorks.FDO.FDOTests;
+using SIL.Utils;
 
 namespace XMLViewsTests
 {
 	public class ConfiguredExportTests : MemoryOnlyBackendProviderRestoredForEachTestTestBase
 	{
-
 		[Test]
 		public void BeginCssClassIfNeeded_UsesSafeClasses()
 		{
 			TestBeginCssClassForFlowType("para");
 			TestBeginCssClassForFlowType("span");
+		}
+
+		[Test]
+		public void XHTMLExportGetDigraphMapsFirstCharactersFromICUSortRules()
+		{
+			var ws = Cache.LangProject.DefaultVernacularWritingSystem;
+			ws.SortRules = "&b < az << a < c <<< ch";
+			ws.SortUsing = WritingSystemDefinition.SortRulesType.CustomICU;
+
+			var exporter = new ConfiguredExport(null, null, 0);
+			string output;
+			using (var stream = new MemoryStream())
+			{
+				using (var writer = new StreamWriter(stream))
+				{
+					exporter.Initialize(Cache, null, writer, null, "xhtml", null, "dicBody");
+					Dictionary<string, string> mapChars;
+					Set<string> ignoreSet;
+					var data = exporter.GetDigraphs(ws.Id, out mapChars, out ignoreSet);
+					Assert.AreEqual(mapChars.Count, 2, "Too many characters found equivalents");
+					Assert.AreEqual(mapChars["a"], "az");
+					Assert.AreEqual(mapChars["ch"], "c");
+				}
+			}
+		}
+
+		[Test]
+		public void XHTMLExportGetDigraphMapsFirstCharactersFromToolboxSortRules()
+		{
+			var ws = Cache.LangProject.DefaultVernacularWritingSystem;
+			ws.SortRules = "b" + Environment.NewLine + "az a" + Environment.NewLine + "c ch";
+			ws.SortUsing = WritingSystemDefinition.SortRulesType.CustomSimple;
+
+			var exporter = new ConfiguredExport(null, null, 0);
+			string output;
+			using (var stream = new MemoryStream())
+			{
+				using (var writer = new StreamWriter(stream))
+				{
+					exporter.Initialize(Cache, null, writer, null, "xhtml", null, "dicBody");
+					Dictionary<string, string> mapChars;
+					Set<string> ignoreSet;
+					var data = exporter.GetDigraphs(ws.Id, out mapChars, out ignoreSet);
+					Assert.AreEqual(mapChars.Count, 2, "Too many characters found equivalents");
+					Assert.AreEqual(mapChars["a"], "az");
+					Assert.AreEqual(mapChars["ch"], "c");
+				}
+			}
 		}
 
 		private void TestBeginCssClassForFlowType(string flowType)
