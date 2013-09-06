f9dd95a3cc3ea4bf17e83421508ab50ac784fec3
Stephen McConnel <stephen_mcconnel@sil.org>
2013-09-06 16:12:21 -0500

    Fix browser interaction for Try a Word dialog (FWNX-725)
    Either changing to geckofx-14 or recent fixes to marshalling seems to have
    fixed the crash that prevented the bugfix from being viable.  Two more
    buttons that were being ignored in the word grammar debugger are also
    fixed.
    
    Change-Id: I8665e3e6e63613baf5cb197198f3ffef7070829b

--- Src/LexText/ParserUI/WebPageInteractor.cs
+++ Src/LexText/ParserUI/WebPageInteractor.cs
@@ -77,19 +77,12 @@ namespace SIL.FieldWorks.LexText.Controls
 			if (sender == null || e == null || e.Target == null)
 				return;
 
+			GeckoNode onClick = null;
 			GeckoElement parentTable = GetParentTable(e.Target);
-
-			if (parentTable == null)
-				return;
-
-			GeckoNode onClick = parentTable.Attributes["onclick"];
-			// The next two lines are needed to fix FWNX-725, but if they're not commented out, the
-			// program silently disappears shortly after the TryAWordDlg dialog is closed *if this
-			// method is ever invoked by clicking on the HTML control anywhere*.  Somehow, either
-			// e.Target.Attributes["onclick"] or onClick.TextContent must set some state in the browser
-			// that causes this horrendous behavior.
-			//if (onClick == null)
-			//	onClick = e.Target.Attributes["onclick"];
+			if (parentTable != null)
+				onClick = parentTable.Attributes["onclick"];
+			if (onClick == null)
+				onClick = e.Target.Attributes["onclick"];
 			if (onClick == null)
 				return;
 
@@ -102,11 +95,11 @@ namespace SIL.FieldWorks.LexText.Controls
 			{
 				ShowWordGrammarDetail(GetParameterFromJavaScriptFunctionCall(js));
 			}
-			if (js.Contains("TryWordGrammarAgain"))
+			if (js.Contains("TryWordGrammarAgain") || js.Contains("ButtonTryNextPass"))
 			{
 				TryWordGrammarAgain(GetParameterFromJavaScriptFunctionCall(js));
 			}
-			if (js.Contains("GoToPreviousWordGrammarPage"))
+			if (js.Contains("GoToPreviousWordGrammarPage") || js.Contains("ButtonGoBack()"))
 			{
 				GoToPreviousWordGrammarPage();
 			}
