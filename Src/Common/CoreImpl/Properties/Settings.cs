481a2b9de4bb7a4a2e76c0cabb3f87ab8b1b8a28
jason_naylor <jason_naylor@sil.org>
2013-10-30 12:05:43 -0500

    Modify FLEx to use the CrossPlatformSettingsProvider class
    * Add the Provider property to each setting
    * Add a Settings class to catch and warn developers if
      they add new settings without using the new provider
    
    Change-Id: Ifeda74c66d42483896c49d8fedb942b61629d200

---
+++ Src/Common/CoreImpl/Properties/Settings.cs
@@ -0,0 +1,29 @@
+using System.Configuration;
+using System.Diagnostics;
+using Palaso.Settings;
+
+namespace SIL.CoreImpl.Properties
+{
+
+	/// <summary>
+	/// Settings class to put a custom provider in.
+	/// </summary>
+	public sealed partial class Settings
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Settings"/> class setting
+		/// the provider collection to contain a CrossPlatformSettingsProvider and making every property
+		/// use that.
+		/// </summary>
+		public Settings()
+		{
+			foreach(SettingsProperty property in Properties)
+			{
+				if(!(property.Provider is CrossPlatformSettingsProvider))
+				{
+					Debug.Assert(property.Provider is CrossPlatformSettingsProvider, "Property '" + property.Name + "' Needs the Provider string set to CrossPlatformSettingsProvider");
+				}
+			}
+		}
+	}
+}
