68ff26c53ebfe632dfb84bedf9c46494a7754e63
MarkS <marksvc@gmail.com>
2013-12-12 12:56:35 -0700

    Remove double space
    Change-Id: I0edf043393d4f7c702017fc74048f6cc9a55af25

--- Src/bldinc.h
+++ Src/bldinc.h
@@ -16,4 +16,4 @@
 #define STR_PRODUCT "$!{FWMAJOR:0}.$!{FWMINOR:0}.$!{FWREVISION:0}.$NUMBEROFDAYS\0"
 #define FWSUITE_VERSION "$!{FWMAJOR:0}.$!{FWMINOR:0}.$!{FWREVISION:0}.0\0"
 #define COPYRIGHT "Copyright © 2002-$YEAR SIL International\0"
-#define COPYRIGHTRESERVED "Copyright © 2002-$YEAR SIL International.  All rights reserved."
+#define COPYRIGHTRESERVED "Copyright © 2002-$YEAR SIL International. All rights reserved."
