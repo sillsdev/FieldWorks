694190f00d191bf27ae14a20850c167e6d5160f2
Hasso Pape <hasso_pape@sil.org>
2014-02-14 10:21:41 -0600

    Add model for special configuration options
    Change-Id: Ibc73db64f9d6de732820f7488e9de90f6c56713b

--- Src/xWorks/DictionaryNodeOptions.cs
+++ Src/xWorks/DictionaryNodeOptions.cs
@@ -2,14 +2,83 @@
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
 namespace SIL.FieldWorks.XWorks
 {
 	/// <summary>
-	/// Abstract class representing ConfigurableDictionaryNode options.
+	/// Base class for ConfigurableDictionaryNode options
 	/// <note>This would be an interface, but the XMLSerialization doesn't like those</note>
 	/// </summary>
-	public abstract class DictionaryNodeOptions
-	{
+	public abstract class DictionaryNodeOptions {}
 
+	/// <summary>Options for formatting Senses</summary>
+	public class DictionaryNodeSenseOptions : DictionaryNodeOptions
+	{
+		[XmlAttribute(AttributeName = "numberBefore")]
+		public string BeforeNumber { get; set; }
+
+		// Valid values: ""->none; %O->1.2.3; %z->1, b, iii
+		[XmlAttribute(AttributeName = "numberMark")]
+		public string NumberMark { get; set; }
+
+		[XmlAttribute(AttributeName = "numberAfter")]
+		public string AfterNumber { get; set; }
+
+		// currently represents bold, italic, and font
+		[XmlAttribute(AttributeName = "numberStyle")]
+		public string NumberStyle { get; set; }
+
+		[XmlAttribute(AttributeName = "numberSingleSense")]
+		public bool NumberEvenASingleSense { get; set; }
+
+		[XmlAttribute(AttributeName = "showSingleGramInfoFirst")]
+		public bool ShowSharedGrammarInfoFirst { get; set; }
+
+		[XmlAttribute(AttributeName = "displayEachSenseInParagraph")]
+		public bool DisplayEachSenseInAParagraph { get; set; }
+	}
+
+	/// <summary>Options for selecting and ordering items in lists, including Custom Lists and WritingSystem lists</summary>
+	public class DictionaryNodeListOptions : DictionaryNodeOptions
+	{
+		public class DictionaryNodeOption
+		{
+			[XmlIgnore]
+			public string Label { get; set; }
+			[XmlAttribute(AttributeName = "id")]
+			public string Id { get; set; }
+			[XmlAttribute(AttributeName = "isEnabled")]
+			public bool IsEnabled { get; set; }
+		}
+
+		[XmlElement(ElementName = "Options")]
+		public List<DictionaryNodeOption> Options { get; set; }
+	}
+
+	/// <summary>Options for selecting and ordering WritingSystems</summary>
+	public class DictionaryNodeWritingSystemOptions : DictionaryNodeListOptions
+	{
+		public enum WritingSystemType
+		{
+			[XmlEnum("vernacular")]
+			Vernacular,
+			[XmlEnum("analysis")]
+			Analysis,
+			//both Analysis and Vernacular
+			[XmlEnum("both")]
+			Both,
+			[XmlEnum("pronunciation")]
+			Pronunciation
+		}
+
+		// REVIEW (Hasso) 2014.02: since this never changes, we may not need it in the schema; however,
+		// we will somehow need to determine which writing systems are available for each instance
+		[XmlAttribute(AttributeName = "writingSystemType")]
+		public WritingSystemType WsType { get; set; }
+
+		[XmlAttribute(AttributeName = "displayWSAbreviation")]
+		public bool DisplayWritingSystemAbbreviations { get; set; }
 	}
 }
