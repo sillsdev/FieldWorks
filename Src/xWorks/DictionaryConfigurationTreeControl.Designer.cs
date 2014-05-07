c4df3ffcd4775721b9639f1eb3658be0e5395d10
MarkS <marksvc@gmail.com>
2014-05-07 11:47:35 -0600

    Add copyright banners
    Change-Id: Ic2ffa07dac1bb6665289c302b3fdff0cf4495b32

--- Src/xWorks/DictionaryConfigurationTreeControl.Designer.cs
+++ Src/xWorks/DictionaryConfigurationTreeControl.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace SIL.FieldWorks.XWorks
+﻿// Copyright (c) 2014 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+namespace SIL.FieldWorks.XWorks
 {
 	partial class DictionaryConfigurationTreeControl
 	{
