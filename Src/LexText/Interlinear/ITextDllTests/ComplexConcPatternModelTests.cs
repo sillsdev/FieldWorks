f55a020115838b35275457a12184ffc7c8be2882
Damien Daspit <damien_daspit@sil.org>
2014-10-02 18:00:24 +0700

    LT-15641: Crash on complex concordance using infl features
    * add a default "unspecified" value to all inflectional features
    * update matcher to use defaults when matching
    
    Change-Id: Id957f26d426952830a2fbef5601c98298301ba20

--- Src/LexText/Interlinear/ITextDllTests/ComplexConcPatternModelTests.cs
+++ Src/LexText/Interlinear/ITextDllTests/ComplexConcPatternModelTests.cs
@@ -7,6 +7,7 @@ using SIL.FieldWorks.FDO;
 using SIL.FieldWorks.FDO.DomainServices;
 using SIL.FieldWorks.FDO.FDOTests;
 using SIL.FieldWorks.FDO.Infrastructure;
+using FS = System.Collections.Generic.Dictionary<SIL.FieldWorks.FDO.IFsFeatDefn, object>;
 
 namespace SIL.FieldWorks.IText
 {
@@ -19,6 +20,7 @@ namespace SIL.FieldWorks.IText
 		private FDO.IText m_text;
 		private ICmPossibility m_np;
 		private IEqualityComparer<IParaFragment> m_fragmentComparer;
+		private IFsFeatStrucType m_inflType;
 
 		[TestFixtureSetUp]
 		public override void FixtureSetup()
@@ -33,17 +35,22 @@ namespace SIL.FieldWorks.IText
 					m_verb = MakePartOfSpeech("verb");
 					m_adj = MakePartOfSpeech("adj");
 
+					IFsFeatureSystem msFeatSys = Cache.LanguageProject.MsFeatureSystemOA;
+					m_inflType = AddFSType(msFeatSys, "infl",
+						AddComplexFeature(msFeatSys, "nounAgr", AddClosedFeature(msFeatSys, "num", "sg", "pl")),
+						AddClosedFeature(msFeatSys, "tense", "pres"));
+
 					m_np = Cache.LangProject.GetDefaultTextTagList().ReallyReallyAllPossibilities.Single(poss => poss.Abbreviation.BestAnalysisAlternative.Text == "Noun Phrase");
 
 					ILexEntry ni = MakeEntry("ni-", m_verb, "1SgSubj");
 					ILexEntry him = MakeEntry("him-", m_verb, "3SgObj");
 					ILexEntry bili = MakeEntry("bili", m_verb, "to see");
-					ILexEntry ra = MakeEntry("-ra", m_verb, "Pres");
+					ILexEntry ra = MakeEntry("-ra", m_verb, "Pres", new FS {{GetFeature("tense"), GetValue("pres")}});
 
 					ILexEntry pus = MakeEntry("pus", m_adj, "green");
 
 					ILexEntry yalo = MakeEntry("yalo", m_noun, "mat");
-					ILexEntry la = MakeEntry("-la", m_noun, "1SgPoss");
+					ILexEntry la = MakeEntry("-la", m_noun, "1SgPoss", new FS {{GetFeature("nounAgr"), new FS {{GetFeature("num"), GetValue("sg")}}}});
 
 					MakeWordform("nihimbilira", "I see", m_verb, ni, him, bili, ra);
 					MakeWordform("pus", "green", m_adj, pus);
@@ -54,6 +61,83 @@ namespace SIL.FieldWorks.IText
 					var para = (IStTxtPara) m_text.ContentsOA.ParagraphsOS.First();
 					MakeTag(m_text, m_np, para.SegmentsOS.First(), 1, para.SegmentsOS.First(), 3);
 				});
+		}
+
+		private IFsClosedFeature AddClosedFeature(IFsFeatureSystem featSys, string name, params string[] values)
+		{
+			IFsClosedFeature feat = Cache.ServiceLocator.GetInstance<IFsClosedFeatureFactory>().Create();
+			featSys.FeaturesOC.Add(feat);
+			feat.Name.SetAnalysisDefaultWritingSystem(name);
+			feat.Abbreviation.SetAnalysisDefaultWritingSystem(name);
+			foreach (string value in values)
+			{
+				IFsSymFeatVal symbol = Cache.ServiceLocator.GetInstance<IFsSymFeatValFactory>().Create();
+				feat.ValuesOC.Add(symbol);
+				symbol.Name.SetAnalysisDefaultWritingSystem(value);
+				symbol.Abbreviation.SetAnalysisDefaultWritingSystem(value);
+			}
+			return feat;
+		}
+
+		private IFsFeatStrucType AddFSType(IFsFeatureSystem featSys, string name, params IFsFeatDefn[] features)
+		{
+			IFsFeatStrucType type = Cache.ServiceLocator.GetInstance<IFsFeatStrucTypeFactory>().Create();
+			featSys.TypesOC.Add(type);
+			type.Name.SetAnalysisDefaultWritingSystem(name);
+			type.Abbreviation.SetAnalysisDefaultWritingSystem(name);
+			foreach (IFsFeatDefn fd in features)
+				type.FeaturesRS.Add(fd);
+			return type;
+		}
+
+		private IFsComplexFeature AddComplexFeature(IFsFeatureSystem featSys, string name, params IFsFeatDefn[] features)
+		{
+			IFsFeatStrucType type = AddFSType(featSys, name, features);
+
+			IFsComplexFeature feat = Cache.ServiceLocator.GetInstance<IFsComplexFeatureFactory>().Create();
+			featSys.FeaturesOC.Add(feat);
+			feat.Name.SetAnalysisDefaultWritingSystem(name);
+			feat.Abbreviation.SetAnalysisDefaultWritingSystem(name);
+			feat.TypeRA = type;
+			return feat;
+		}
+
+		private void CreateFeatStruc(IFsFeatStrucType type, IFsFeatStruc fs, FS featVals)
+		{
+			fs.TypeRA = type;
+			foreach (KeyValuePair<IFsFeatDefn, object> featVal in featVals)
+			{
+				var closedFeat = featVal.Key as IFsClosedFeature;
+				if (closedFeat != null)
+				{
+					var sym = (IFsSymFeatVal) featVal.Value;
+					IFsClosedValue cv = Cache.ServiceLocator.GetInstance<IFsClosedValueFactory>().Create();
+					fs.FeatureSpecsOC.Add(cv);
+					cv.FeatureRA = closedFeat;
+					cv.ValueRA = sym;
+				}
+				else
+				{
+					var complexFeat = (IFsComplexFeature) featVal.Key;
+					IFsComplexValue cv = Cache.ServiceLocator.GetInstance<IFsComplexValueFactory>().Create();
+					fs.FeatureSpecsOC.Add(cv);
+
+					IFsFeatStruc childFS = Cache.ServiceLocator.GetInstance<IFsFeatStrucFactory>().Create();
+					cv.FeatureRA = complexFeat;
+					cv.ValueOA = childFS;
+					CreateFeatStruc(complexFeat.TypeRA, childFS, (FS) featVal.Value);
+				}
+			}
+		}
+
+		private IFsFeatDefn GetFeature(string id)
+		{
+			return Cache.LanguageProject.MsFeatureSystemOA.FeaturesOC.First(f => f.Abbreviation.AnalysisDefaultWritingSystem.Text == id);
+		}
+
+		private IFsSymFeatVal GetValue(string id)
+		{
+			return Cache.LanguageProject.MsFeatureSystemOA.FeaturesOC.OfType<IFsClosedFeature>().SelectMany(f => f.ValuesOC).First(sym => sym.Abbreviation.AnalysisDefaultWritingSystem.Text == id);
 		}
 
 		private void MakeTag(FDO.IText text, ICmPossibility tag, ISegment beginSeg, int begin, ISegment endSeg, int end)
@@ -111,7 +195,7 @@ namespace SIL.FieldWorks.IText
 			return slotType;
 		}
 
-		private ILexEntry MakeEntry(string lf, IPartOfSpeech pos, string gloss)
+		private ILexEntry MakeEntry(string lf, IPartOfSpeech pos, string gloss, FS inflFS = null)
 		{
 			// The entry itself.
 			ILexEntry entry = Cache.ServiceLocator.GetInstance<ILexEntryFactory>().Create();
@@ -119,7 +203,7 @@ namespace SIL.FieldWorks.IText
 
 			Guid slotType = GetSlotType(lf);
 			IMoForm form;
-			IMoMorphSynAnalysis msa = GetMsaAndMoForm(entry, slotType, pos, out form);
+			IMoMorphSynAnalysis msa = GetMsaAndMoForm(entry, slotType, pos, inflFS, out form);
 			entry.LexemeFormOA = form;
 			string trimmed = lf.Trim('-');
 			form.Form.VernacularDefaultWritingSystem = MakeVernString(trimmed);
@@ -133,8 +217,9 @@ namespace SIL.FieldWorks.IText
 			return entry;
 		}
 
-		private IMoMorphSynAnalysis GetMsaAndMoForm(ILexEntry entry, Guid slotType, IPartOfSpeech pos, out IMoForm form)
+		private IMoMorphSynAnalysis GetMsaAndMoForm(ILexEntry entry, Guid slotType, IPartOfSpeech pos, FS inflFS, out IMoForm form)
 		{
+			IFsFeatStruc fs = inflFS == null ? null : Cache.ServiceLocator.GetInstance<IFsFeatStrucFactory>().Create();
 			IMoMorphSynAnalysis msa;
 			if (slotType == MoMorphTypeTags.kguidMorphStem)
 			{
@@ -143,6 +228,8 @@ namespace SIL.FieldWorks.IText
 				msa = stemMsa;
 				entry.MorphoSyntaxAnalysesOC.Add(msa);
 				stemMsa.PartOfSpeechRA = pos;
+				if (inflFS != null)
+					stemMsa.MsFeaturesOA = fs;
 			}
 			else
 			{
@@ -151,7 +238,11 @@ namespace SIL.FieldWorks.IText
 				msa = affixMsa;
 				entry.MorphoSyntaxAnalysesOC.Add(msa);
 				affixMsa.PartOfSpeechRA = pos;
+				if (inflFS != null)
+					affixMsa.InflFeatsOA = fs;
 			}
+			if (inflFS != null)
+				CreateFeatStruc(m_inflType, fs, inflFS);
 			return msa;
 		}
 
@@ -197,6 +288,16 @@ namespace SIL.FieldWorks.IText
 			model.Root.Children.Add(new ComplexConcWordNode {Category = m_verb});
 			model.Compile();
 			Assert.That(model.Search(m_text.ContentsOA), Is.EquivalentTo(new IParaFragment[] {new ParaFragment(seg, 0, 11, null)}).Using(m_fragmentComparer));
+
+			model.Root.Children.Clear();
+			model.Root.Children.Add(new ComplexConcWordNode {InflFeatures = {{GetFeature("nounAgr"), new FS {{GetFeature("num"), new ClosedFeatureValue(GetValue("sg"), false)}}}}});
+			model.Compile();
+			Assert.That(model.Search(m_text.ContentsOA), Is.EquivalentTo(new IParaFragment[] {new ParaFragment(seg, 17, 23, null)}).Using(m_fragmentComparer));
+
+			model.Root.Children.Clear();
+			model.Root.Children.Add(new ComplexConcWordNode {InflFeatures = {{GetFeature("tense"), new ClosedFeatureValue(GetValue("pres"), true)}}});
+			model.Compile();
+			Assert.That(model.Search(m_text.ContentsOA), Is.EquivalentTo(new IParaFragment[] {new ParaFragment(seg, 12, 15, null), new ParaFragment(seg, 17, 23, null)}).Using(m_fragmentComparer));
 
 			model.Root.Children.Clear();
 			model.Root.Children.Add(new ComplexConcMorphNode {Category = m_noun});
