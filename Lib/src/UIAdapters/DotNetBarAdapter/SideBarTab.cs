91b60c0f12ede0b6e45466f467b7a8ee5c8cbc78
Hasso Pape <hasso_pape@sil.org>
2015-04-27 16:02:26 -0500

    Update some copyright notices to LGPL
    and remove redundant #region // copyright \r\n // <copyright
    and their end tags.
    
    Change-Id: Iae3697c883ea508b8759093388c3fcadc1c73a78

--- Lib/src/UIAdapters/DotNetBarAdapter/SideBarTab.cs
+++ Lib/src/UIAdapters/DotNetBarAdapter/SideBarTab.cs
@@ -1,12 +1,7 @@
 // ---------------------------------------------------------------------------------------------
-#region // Copyright (c) 2005-2008, SIL International. All Rights Reserved.
-// <copyright from='2005' to='2008' company='SIL International'>
-//		Copyright (c) 2005-2008, SIL International. All Rights Reserved.
-//
-//		Distributable under the terms of either the Common Public License or the
-//		GNU Lesser General Public License, as specified in the LICENSING.txt file.
-// </copyright>
-#endregion
+// Copyright (c) 2005-2015 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
 //
 // File: SideBarTab.cs
 // Responsibility: DavidO
--- Lib/src/UIAdapters/SilSidePaneAdapter/UIAdaptersTestClient/MainWindow.cs
+++ Lib/src/UIAdapters/SilSidePaneAdapter/UIAdaptersTestClient/MainWindow.cs
@@ -1,12 +1,7 @@
 // --------------------------------------------------------------------------------------------
-#region // Copyright (c) 2009, SIL International. All Rights Reserved.
-// <copyright from='2009' to='2009' company='SIL International'>
-//		Copyright (c) 2009, SIL International. All Rights Reserved.
-//
-//		Distributable under the terms of either the Common Public License or the
-//		GNU Lesser General Public License, as specified in the LICENSING.txt file.
-// </copyright>
-#endregion
+// Copyright (c) 2009-2015 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
 //
 // File:
 // Responsibility:
