69a0db86824716048b785435522ce39ccaefe7fe
Ken Zook <ken_zook@sil.org>
2015-06-09 07:48:43 -0500

    Increase timeout value for ScrControlsTests
    Change-Id: Ifad369612f7e148f755dc9539ce4940087a02967

--- Build/Src/FwBuildTasks/CollectTargets.cs
+++ Build/Src/FwBuildTasks/CollectTargets.cs
@@ -499,6 +499,7 @@ namespace FwBuildTasks
 				case "BasicUtilsTests":			// ~24 sec (overnight 2/27/15)
 				case "LexEdDllTests":		        // ~24 sec (overnight 4/15/15)
 				case "CheckAdminPrivilege":			// ~23 sec (overnight 6/8/15)
+				case "ScrControlsTests":			// ~21 sec (overnight 6/9/15)
 				case "ParserCoreTests":
 					return 40000;
 				case "PhraseTranslationHelperTests":	// ~8 sec
