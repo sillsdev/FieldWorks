eb50af3401ad39de334236b65441ff29f7069839
Hasso Pape <hasso_pape@sil.org>
2015-07-29 15:28:55 -0500

    Fix copyright notices in ControlLibrary
    Change-Id: I9fe949a56e340a4e76705e74e8a4dea531c79587

--- Src/XCore/ControlLibrary/SidebarLibrary/Win32/RegistryUtil.cs
+++ Src/XCore/ControlLibrary/SidebarLibrary/Win32/RegistryUtil.cs
@@ -1,6 +1,4 @@
-// Copyright (c) 2015 SIL International
-// This software is licensed under the LGPL, version 2.1 or later
-// (http://www.gnu.org/licenses/lgpl-2.1.html)
+// Original author or copyright holder unknown.
 
 using System;
 using Microsoft.Win32;
