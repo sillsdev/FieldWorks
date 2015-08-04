50ce9380832dbaff789c0fa9d4c7eed2185d7f61
Hasso Pape <hasso_pape@sil.org>
2015-08-04 17:35:13 -0500

    LT-14629: Refresh Inflection Features after Insertion
    Refresh the Inflection Features edit pane after
    insertion or deletion of a Feature Value.
    
    Change-Id: I4eb90c1e68c6bd1e77b951c758616be2474c4a33

--- Src/FDO/DomainImpl/OverridesCellar.cs
+++ Src/FDO/DomainImpl/OverridesCellar.cs
@@ -1943,6 +1943,27 @@ namespace SIL.FieldWorks.FDO.DomainImpl
 			}
 		}
 
+		protected override void AddObjectSideEffectsInternal(AddObjectEventArgs e)
+		{
+			switch(e.Flid)
+			{
+				case FsClosedFeatureTags.kflidValues:
+					m_cache.ServiceLocator.GetInstance<IUnitOfWorkService>().RegisterVirtualAsModified(this, "ValuesSorted", ValuesSorted);
+					break;
+			}
+			base.AddObjectSideEffectsInternal(e);
+		}
+
+		protected override void RemoveObjectSideEffectsInternal(RemoveObjectEventArgs e)
+		{
+			switch(e.Flid)
+			{
+				case FsClosedFeatureTags.kflidValues:
+					m_cache.ServiceLocator.GetInstance<IUnitOfWorkService>().RegisterVirtualAsModified(this, "ValuesSorted", ValuesSorted);
+					break;
+			}
+			base.RemoveObjectSideEffectsInternal(e);
+		}
 	}
 	#endregion
 
