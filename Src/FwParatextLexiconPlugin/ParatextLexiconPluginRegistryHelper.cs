4d89cf21c14fc122764555f3f8fa5dd42deb8bf4
Damien Daspit <damien_daspit@sil.org>
2015-08-26 11:09:08 +0700

    Copy PT registry from correct location when installed
    * PT does not append the version number to its config directory when
    installed
    
    Change-Id: Ibb6a4c591f109ddde2acda4371b6fc9c4e94fbfe

--- Src/FwParatextLexiconPlugin/ParatextLexiconPluginRegistryHelper.cs
+++ Src/FwParatextLexiconPlugin/ParatextLexiconPluginRegistryHelper.cs
@@ -21,17 +21,19 @@ namespace SIL.FieldWorks.ParatextLexiconPlugin
 			if (MiscUtils.IsUnix)
 			{
 				const string fwRegKey = "LocalMachine/software/sil";
-
-				string ptRegLoc = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), ".config/paratext7.6/registry", fwRegKey);
-
 #if DEBUG
 				// On a developer Linux machine these are kept under output/registry. Since the program is running at output/{debug|release},
 				// one level up should find the registry folder.
 				string fwRegLoc = Path.Combine(Path.GetDirectoryName(FileUtils.StripFilePrefix(Assembly.GetExecutingAssembly().CodeBase)) ?? ".",
 					"../registry", fwRegKey);
+				// On a developer Linux machine, PT appends the version number to its config directory, which contains the registry
+				string paratextVersion = "7.6";
 #else
 				string fwRegLoc = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), ".config/fieldworks/registry", fwRegKey);
+				string paratextVersion = "";
 #endif
+				string ptRegLoc = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					string.Format(".config/paratext{0}/registry", paratextVersion), fwRegKey);
 
 				if (Directory.Exists(fwRegLoc))
 					DirectoryUtils.CopyDirectory(fwRegLoc, ptRegLoc, true, true);
