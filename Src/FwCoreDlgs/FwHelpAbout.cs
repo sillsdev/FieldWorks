614fea6c4732941fe60c670fc678c58b659b6d9e
MarkS <marksvc@gmail.com>
2015-09-09 13:22:27 -0400

    Expand Linux package version reporting
    * Addresses LT-16168.
    
    Change-Id: I89536637c13742b86ee9a4ca28583a75384f5eed

--- Src/FwCoreDlgs/FwHelpAbout.cs
+++ Src/FwCoreDlgs/FwHelpAbout.cs
@@ -15,6 +15,8 @@ using SIL.FieldWorks.Common.FwUtils;
 using SIL.Utils;
 using System.Diagnostics;
 using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SIL.FieldWorks.FwCoreDlgs
 {
@@ -111,9 +113,24 @@ namespace SIL.FieldWorks.FwCoreDlgs
 				this.Controls.Add(packageVersionLabel);
 				this.Controls.Add(versionInformation);
 
-				foreach(var info in LinuxPackageUtils.FindInstalledPackages("fieldworks*"))
+				foreach(var info in LinuxPackageUtils.FindInstalledPackages("fieldworks")
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-applications"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-enc-converters"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("flexbridge"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-l10n-*"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("mono-sil"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("libgdiplus-sil"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("gtk-sharp2-sil"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("mono-basic-sil"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("*geckofx*"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("chmsee"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("pathway"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-mono"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-libgdiplus"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-gtk-sharp2"))
+					.Concat<KeyValuePair<string, string>>(LinuxPackageUtils.FindInstalledPackages("fieldworks-mono-basic")))
 				{
-					versionInformation.AppendText(String.Format("{0} {1} {2}", info.Key, info.Value, Environment.NewLine));
+					versionInformation.AppendText(String.Format("{0} {1}{2}", info.Key, info.Value, Environment.NewLine));
 				}
 			}
 		}
