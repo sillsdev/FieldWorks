90149be9eadc705a90de5046b2598f79fbc1133f
Randy Regnier <rbregnier@gmail.com>
2015-10-07 18:25:27 -0700

    Remove FlexDePlugin and tests
    Greg Tryus says this was used in the past,
    but the Pathway export behavior is now on a main
    Export menu, and is enabled when Pathway is installed.
    
    Change-Id: I87567abbeae4fd3e8923be4692becfcf1fd0a21c

--- Src/LexText/FlexDePlugin/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Resources;
-
-[assembly: AssemblyTitle("FlexDePlugin")]
-[assembly: AssemblyDescription("FLEx utility for converting XHTML to ODT or PDF")]
-
-[assembly: ComVisible(false)]
