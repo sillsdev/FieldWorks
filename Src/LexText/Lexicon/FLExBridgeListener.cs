0358feb5d62c5b5e402606342d6e68b1cd5bf32c
Gordon Martin <gordon_martin@sil.org>
2016-02-02 09:24:14 -0800

    LT-16969 Ensure Dict Config Schema is available for FlexBridge
    Change-Id: I9c31994682a86cad352d84ee699b6022c7aae2b7

--- Src/LexText/Lexicon/FLExBridgeListener.cs
+++ Src/LexText/Lexicon/FLExBridgeListener.cs
@@ -12,6 +12,7 @@ using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
+using Palaso.IO;
 using Palaso.Lift;
 using Palaso.Lift.Migration;
 using Palaso.Lift.Parsing;
@@ -339,18 +340,19 @@ namespace SIL.FieldWorks.XWorks.LexEd
 			string dummy;
 			var fullProjectFileName = Path.Combine(projectFolder, Cache.ProjectId.Name + FdoFileHelper.ksFwDataXmlFileExtension);
 			bool dataChanged;
-			var success = FLExBridgeHelper.LaunchFieldworksBridge(fullProjectFileName, SendReceiveUser,
-																  FLExBridgeHelper.SendReceive,
-																  null, FDOBackendProvider.ModelVersion, "0.13",
-																  Cache.LangProject.DefaultVernacularWritingSystem.Id, null,
-																  out dataChanged, out dummy);
-			if (!success)
+			using (CopyDictionaryConfigFileToTemp(projectFolder))
 			{
-				ReportDuplicateBridge();
-				ProjectLockingService.LockCurrentProject(Cache);
-				return true;
+				var success = FLExBridgeHelper.LaunchFieldworksBridge(fullProjectFileName, SendReceiveUser, FLExBridgeHelper.SendReceive,
+					null, FDOBackendProvider.ModelVersion, "0.13",
+					Cache.LangProject.DefaultVernacularWritingSystem.Id, null,
+					out dataChanged, out dummy);
+				if (!success)
+				{
+					ReportDuplicateBridge();
+					ProjectLockingService.LockCurrentProject(Cache);
+					return true;
+				}
 			}
-
 			if (dataChanged)
 			{
 				bool conflictOccurred = DetectMainConflicts(projectFolder, savedState);
@@ -373,11 +375,28 @@ namespace SIL.FieldWorks.XWorks.LexEd
 		private bool LinkedFilesLocationIsDefault()
 		{
 			var defaultLinkedFilesFolder = FdoFileHelper.GetDefaultLinkedFilesDir(Cache.ServiceLocator.DataSetup.ProjectId.ProjectFolder);
-			if (!defaultLinkedFilesFolder.Equals(Cache.LanguageProject.LinkedFilesRootDir))
-				return false;
-			else
-				return true;
+			return defaultLinkedFilesFolder.Equals(Cache.LanguageProject.LinkedFilesRootDir);
 		}
+
+		// FlexBridge looks for the schema to validate Dictionary Configuration files in the project's Temp directory.
+		private static TempFile CopyDictionaryConfigFileToTemp(string projectFolder)
+		{
+			const string dictConfigSchemaFileName = "DictionaryConfiguration.xsd";
+			var dictConfigSchemaPath = Path.Combine(FwDirectoryFinder.FlexFolder, "Configuration", dictConfigSchemaFileName);
+			var projectTempFolder = Path.Combine(projectFolder, "Temp");
+			var dictConfigSchemaTempPath = Path.Combine(projectTempFolder, dictConfigSchemaFileName);
+			if (!Directory.Exists(projectTempFolder))
+				Directory.CreateDirectory(projectTempFolder);
+			if (File.Exists(dictConfigSchemaTempPath))
+			{
+				// We've had difficulties in the past trying to delete this file while it's read-only. This may apply only to early testers' projects.
+				File.SetAttributes(dictConfigSchemaTempPath, FileAttributes.Normal);
+				File.Delete(dictConfigSchemaTempPath);
+			}
+			File.Copy(dictConfigSchemaPath, dictConfigSchemaTempPath);
+			return new TempFile(dictConfigSchemaTempPath, true);
+		}
+
 		#endregion FLExBridge S/R messages
 
 		#region LiftBridge S/R messages
