b4bee5e8417a3aedbf5a6878c75541d469557871
Jason Naylor <jason_naylor@sil.org>
2016-03-02 07:45:14 -0600

    Prune some dead methods
    Change-Id: Iedbf2e3a6953a17c28bf6e1be7cce658a265f8f3

--- Src/LexText/Lexicon/ReversalListener.cs
+++ Src/LexText/Lexicon/ReversalListener.cs
@@ -586,35 +586,6 @@ namespace SIL.FieldWorks.XWorks.LexEd
 		}
 
 		/// <summary>
-		///
-		/// </summary>
-		/// <param name="argument"></param>
-		public virtual void OnInsertReversalIndex(object argument)
-		{
-			CheckDisposed();
-
-			Guid newGuid = CreateNewReversalIndex();
-			if (newGuid != Guid.Empty)
-			{
-				ChangeOwningObject(newGuid);
-				var guid = ReversalIndexEntryUi.GetObjectGuidIfValid(m_mediator, "ReversalIndexGuid");
-				if (guid.Equals(Guid.Empty) || !guid.Equals(newGuid))
-					SetReversalIndexGuid(newGuid);
-			}
-		}
-
-		private Guid CreateNewReversalIndex()
-		{
-			if (Cache == null)
-				return Guid.Empty;
-			if (Cache.LanguageProject == null)
-				return Guid.Empty;
-			if (Cache.LanguageProject.LexDbOA == null)
-				return Guid.Empty;
-			return Guid.Empty;
-		}
-
-		/// <summary>
 		/// This is enabled whenever the ReversalClerk is active.
 		/// </summary>
 		/// <param name="commandObject"></param>
