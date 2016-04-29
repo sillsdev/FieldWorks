b91aa056230d3f3086cb5e22ee9006f149bb5f4a
Brent McCarthy <brent_mccarthy@sil.org>
2016-04-29 16:08:27 -0600

    LT-17413 ReverseName model change and migration
    * Add ReverseName to LexEntryType as a MultiUnicode field.(ie.
    Name is "Dialectal Variant", ReverseName is "Dialectal Variant
    of").
    * Swap Abbreviation and ReverseAbbr fields.(ie. Abbreviation had
    "dial. var. of", change to "dial. var." and ReverseAbbr becomes
    "dial. var. of").
    
    Change-Id: I47441d2defc55299a3b94ad54c38c97430f66181

--- Src/FDO/DomainServices/DataMigration/DataMigration7000069.cs
+++ Src/FDO/DomainServices/DataMigration/DataMigration7000069.cs
@@ -11,8 +11,13 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 {
 	/// ----------------------------------------------------------------------------------------
 	/// <summary>
-	/// Fix the Restrictions field for both LexEntry and LexSense to be MultiString (AStr)
+	/// 1) Fix the Restrictions field for both LexEntry and LexSense to be MultiString (AStr)
 	/// instead of MultiUnicode (AUni).
+	///
+	/// 2) Add a MultiString (AUni) ReverseName field to LexEntryType, both Variant Type and
+	/// Complex Form Type. The migration fills in the 'en' ws contents with Name.Value + " of".
+	/// Swap Abbreviation and ReverseAbbr fields. By swapping the fields the "of" will be switched
+	/// to the other reference.
 	/// </summary>
 	/// ----------------------------------------------------------------------------------------
 	internal class DataMigration7000069 : IDataMigration
@@ -21,14 +26,15 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 		{
 			DataMigrationServices.CheckVersionNumber(repoDto, 7000068);
 
-			UpdateTags(repoDto);
+			UpdateRestrictions(repoDto);
+			AddReverseNameAndSwapAbbreviationFields(repoDto);
 
 			DataMigrationServices.IncrementVersionNumber(repoDto);
 		}
 
 		// We update every instance of a Restriction in a LexEntry or a LexSense to change from AUni
 		// to AStr.
-		private void UpdateTags(IDomainObjectDTORepository repoDto)
+		private void UpdateRestrictions(IDomainObjectDTORepository repoDto)
 		{
 			foreach (var dto in repoDto.AllInstancesSansSubclasses("LexEntry").Union(repoDto.AllInstancesSansSubclasses("LexSense")))
 			{
@@ -50,6 +56,38 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 				{
 					DataMigrationServices.UpdateDTO(repoDto, dto, data.ToString());
 				}
+			}
+		}
+
+		private void AddReverseNameAndSwapAbbreviationFields(IDomainObjectDTORepository repoDto)
+		{
+			foreach (var dto in repoDto.AllInstancesSansSubclasses("LexEntryType"))
+			{
+				XElement data = XElement.Parse(dto.Xml);
+
+				var nameElt = data.Element("Name");
+				if (nameElt != null)
+				{
+					var nameAUniElt = nameElt.Elements("AUni").FirstOrDefault(elt => elt.Attribute("ws").Value == "en");
+					if (nameAUniElt != null)
+					{
+						var revNameElt = new XElement("ReverseName");
+						var auni = new XElement("AUni");
+						auni.SetAttributeValue("ws", "en");
+						auni.Value = nameAUniElt.Value + " of";
+						revNameElt.Add(auni);
+						data.Add(revNameElt);
+					}
+				}
+
+				var abbrevElt = data.Element("Abbreviation");
+				var revAbbrElt = data.Element("ReverseAbbr");
+				if(abbrevElt != null)
+					abbrevElt.Name = "ReverseAbbr";
+				if(revAbbrElt != null)
+					revAbbrElt.Name = "Abbreviation";
+
+				DataMigrationServices.UpdateDTO(repoDto, dto, data.ToString());
 			}
 		}
 	}
--- Src/FDO/FDOTests/DataMigrationTests/DataMigration7000069Tests.cs
+++ Src/FDO/FDOTests/DataMigrationTests/DataMigration7000069Tests.cs
@@ -40,6 +40,19 @@ namespace SIL.FieldWorks.FDO.FDOTests.DataMigrationTests
 			var dtos = DataMigrationTestServices.ParseProjectFile("DataMigration7000069.xml");
 			IDomainObjectDTORepository dtoRepos = new DomainObjectDtoRepository(7000068, dtos, mockMdc, null, FwDirectoryFinder.FdoDirectories);
 
+			// Make sure Restrictions has no AStr elements prior to migration.
+			foreach (var dto in dtoRepos.AllInstancesSansSubclasses("LexEntry").Union(dtoRepos.AllInstancesSansSubclasses("LexSense")))
+			{
+				var elt = XElement.Parse(dto.Xml);
+				var restrictionsElt = elt.Element("Restrictions");
+				// Some of the LexEntry and LexSense test classes do not contain Restrictions.
+				if (restrictionsElt != null)
+				{
+					CollectionAssert.IsNotEmpty(restrictionsElt.Elements("AUni"));
+					CollectionAssert.IsEmpty(restrictionsElt.Elements("AStr"));
+				}
+			}
+
 			m_dataMigrationManager.PerformMigration(dtoRepos, 7000069, new DummyProgressDlg());
 
 			const string frWs = "fr";
@@ -80,6 +93,89 @@ namespace SIL.FieldWorks.FDO.FDOTests.DataMigrationTests
 			Assert.IsNull(restrictionsElement);
 			var gloss = lastSense.Element("Gloss");
 			Assert.AreEqual(1, gloss.Descendants("AUni").Count()); // didn't change other AUnis
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// Test the migration from version 7000068 to 7000069 from Name to ReverseName field, and swapping of the
+		/// Abbreviation and ReverseAbbr fields.
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		[Test]
+		public void AddReverseNamePropertyToLexEntryType()
+		{
+			var mockMdc = new MockMDCForDataMigration();
+			mockMdc.AddClass(1, "CmObject", null, new List<string> {"CmPossibility"});
+			mockMdc.AddClass(2, "CmPossibility", "CmObject", new List<string>{"LexEntryType"});
+			mockMdc.AddClass(3, "LexEntryType", "CmPossibility", new List<string>());
+
+			var currentFlid = 2000;
+			mockMdc.AddField(++currentFlid, "Name", CellarPropertyType.MultiUnicode, 2);
+			mockMdc.AddField(++currentFlid, "Abbreviation", CellarPropertyType.MultiUnicode, 2);
+			mockMdc.AddField(++currentFlid, "SubPossibilities", CellarPropertyType.OwningSequence, 2);
+			mockMdc.AddField(++currentFlid, "ReverseAbbr", CellarPropertyType.MultiUnicode, 3);
+			mockMdc.AddField(++currentFlid, "ReverseName", CellarPropertyType.MultiUnicode, 3);
+
+			var dtos = DataMigrationTestServices.ParseProjectFile("DataMigration7000069.xml");
+			IDomainObjectDTORepository dtoRepos = new DomainObjectDtoRepository(7000068, dtos, mockMdc, null, FwDirectoryFinder.FdoDirectories);
+
+			// Make sure Variant and Complex Form Type do not have ReverseName prior to migration.
+			foreach (var dto in dtoRepos.AllInstancesSansSubclasses("LexEntryType"))
+			{
+				var elt = XElement.Parse(dto.Xml);
+				Assert.IsNull(elt.Element("ReverseName"));
+			}
+
+			m_dataMigrationManager.PerformMigration(dtoRepos, 7000069, new DummyProgressDlg());
+
+			const string frWs = "fr";
+			const string enWs = "en";
+			var firstEntry = XElement.Parse(dtoRepos.AllInstancesSansSubclasses("LexEntryType").First().Xml);
+
+			var nameElement = firstEntry.Element("Name");
+			var multiUniElements = nameElement.Descendants("AUni");
+			Assert.AreEqual(1, multiUniElements.Count());
+			var uniString = multiUniElements.FirstOrDefault().Value;
+			Assert.AreEqual("Dialectal Variant", uniString);
+
+			var reversenameElement = firstEntry.Element("ReverseName");
+			multiUniElements = reversenameElement.Descendants("AUni");
+			Assert.AreEqual(1, multiUniElements.Count());
+			uniString = multiUniElements.FirstOrDefault().Value;
+			Assert.AreEqual("Dialectal Variant of", uniString);
+			var attr = multiUniElements.FirstOrDefault().FirstAttribute;
+			Assert.AreEqual("ws", attr.Name.ToString());
+			Assert.AreEqual(enWs, attr.Value);
+
+			// Past is a subpossibility and also has multiple language strings.
+			var pastEntry = XElement.Parse(dtoRepos.AllInstancesSansSubclasses("LexEntryType").First(
+											e => e.Guid.ToString()=="837ebe72-8c1d-4864-95d9-fa313c499d78").Xml);
+
+			// We only test the English contents. Transforming "of" from any language and predicting the outcome
+			// from the Name.Value would be near impossible.
+			reversenameElement = pastEntry.Element("ReverseName");
+			multiUniElements = reversenameElement.Descendants("AUni");
+			Assert.AreEqual(1, multiUniElements.Count());
+			uniString = multiUniElements.FirstOrDefault().Value;
+			Assert.AreEqual("Past of", uniString);
+			attr = multiUniElements.FirstOrDefault().FirstAttribute;
+			Assert.AreEqual("ws", attr.Name.ToString());
+			Assert.AreEqual(enWs, attr.Value);
+
+			var abbrElement = pastEntry.Element("Abbreviation");
+			multiUniElements = abbrElement.Descendants("AUni");
+			Assert.AreEqual(2, multiUniElements.Count());
+			uniString = multiUniElements.First(wselt => wselt.Attribute("ws").Value == "en").Value;
+			Assert.AreEqual("pst.", uniString);
+			uniString = multiUniElements.First(wselt => wselt.Attribute("ws").Value == "fr").Value;
+			Assert.AreEqual("pss.", uniString);
+			var revAbbrElement = pastEntry.Element("ReverseAbbr");
+			multiUniElements = revAbbrElement.Descendants("AUni");
+			Assert.AreEqual(2, multiUniElements.Count());
+			uniString = multiUniElements.First(wselt => wselt.Attribute("ws").Value == "en").Value;
+			Assert.AreEqual("pst. of", uniString);
+			uniString = multiUniElements.First(wselt => wselt.Attribute("ws").Value == "fr").Value;
+			Assert.AreEqual("pss. de", uniString);
 		}
 	}
 }
