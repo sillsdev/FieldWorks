6ef1941779544d1aec4ea0959e2a76663704ad4a
Jason Naylor <jason_naylor@sil.org>
2017-01-19 09:22:04 -0600

    LT-18073: Crash refreshing disposed Dictionary Preview
    If the Preview has been disposed when we attempt to ShowRecordOnIdle,
    don't try to refresh.
    
    Also, don't refresh while the user is holding a key down
    (e.g. holding Backspace to delete a bunch of text)
    
    (cherry picked from commit 9ad6692ef075b4c8bf05010322c9fdbdfe0c0430)
    
    Change-Id: I970748a1cf2ae1489547cf8c69e9423064b1f7c8

--- Src/Utilities/BasicUtils/UserActivityMonitor.cs
+++ Src/Utilities/BasicUtils/UserActivityMonitor.cs
@@ -1,10 +1,12 @@
-﻿// Copyright (c) 2015 SIL International
+﻿// Copyright (c) 2015-2017 SIL International
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace SIL.Utils
 {
@@ -16,6 +18,8 @@ namespace SIL.Utils
 	public class UserActivityMonitor : IMessageFilter
 	{
 		private IntPtr m_lastMousePosition;
+		private DateTime m_lastActivityTime;
+		private bool m_isKeyDown;
 
 		/// <summary>
 		/// Starts monitoring user activity.
@@ -39,7 +43,16 @@ namespace SIL.Utils
 		/// <value>
 		/// The last activity user time.
 		/// </value>
-		public DateTime LastActivityTime { get; private set; }
+		public DateTime LastActivityTime {
+			get
+			{
+#if !__MonoCS__ // Keyboard doesn't exist in Mono. Linux users must prefer voice-to-text. If this leaves a bug in mono, it needs its own solution
+				if (Enum.GetValues(typeof(Key)).Cast<Key>().Any(key => key > 0 && Keyboard.IsKeyDown(key)))
+					return DateTime.Now; // If the user is holding down e.g. the Backspace key, that counts as current activity
+#endif
+				return m_lastActivityTime;
+			}
+		}
 
 		bool IMessageFilter.PreFilterMessage(ref Message m)
 		{
@@ -48,16 +61,15 @@ namespace SIL.Utils
 				// For mouse move, we get spurious ones when it didn't really move. So check the actual position.
 				if (m.LParam != m_lastMousePosition)
 				{
-					LastActivityTime = DateTime.Now;
+					m_lastActivityTime = DateTime.Now;
 					m_lastMousePosition = m.LParam;
-					// Enhance JohnT: suppress ones where it doesn't move??
 				}
 				return false;
 			}
 			if ((m.Msg >= (int) Win32.WinMsgs.WM_MOUSE_Min && m.Msg <= (int) Win32.WinMsgs.WM_MOUSE_Max)
 				|| (m.Msg >= (int) Win32.WinMsgs.WM_KEY_Min && m.Msg <= (int) Win32.WinMsgs.WM_KEY_Max))
 			{
-				LastActivityTime = DateTime.Now;
+				m_lastActivityTime = DateTime.Now;
 			}
 			return false; // don't want to block any messages.
 		}
