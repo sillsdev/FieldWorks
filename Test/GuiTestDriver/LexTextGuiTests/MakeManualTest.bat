0c10766d114667febd7bcd8cb439f463334918ec
Randy Regnier <rbregnier@gmail.com>
2017-09-22 09:34:24 -0700

    LT-18652 - Remove Deadwood (Round 5): Delete "Test" folder & contents
    Change-Id: I005f4247ba46b84ca5182f67ca00d90b4b6f3db4

--- Test/GuiTestDriver/LexTextGuiTests/MakeManualTest.bat
+++
@@ -1,6 +0,0 @@
-rem Make a manual test html file from an accil script.
-
-rem name the script file without an extension.
-set ScriptName=tlsDelete
-
-c:\MSXSL.EXE %ScriptName%.xml MakeManual.xsl -o %ScriptName%.htm
--- Test/VT/DN/fwvtDataNotebook.scm
+++
@@ -1,6 +0,0 @@
-:FD:\Work\fw\Test\VT\DN\
-:FGeneral
-:1
-General-GUI-1-0-2.mst
-:
-:F..
