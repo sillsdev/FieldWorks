sillsdev/FieldWorks

commit fd6744f9979d78e360547f80834dfafd0ec3f2bf

Another attempt to re-enable parallel processing of projects
Change-Id: I593fd8e983db1dd80c16a178ddf302d49884b0d1

Build/Src/FwBuildTasks/Localization/ProjectLocalizer.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Xml.Linq;
 using FwBuildTasks;
 using Microsoft.Build.Framework;
@@ -198,20 +199,51 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 		{
 			// Run assembly linker with the specified arguments
 			Directory.CreateDirectory(Path.GetDirectoryName(outputDllPath)); // make sure the directory in which we want to make it exists.
-			using (var process = new Process())
+			var fileName = IsUnix ? "al" : "al.exe";
+			var arguments = BuildLinkerArgs(outputDllPath, culture, fileversion,
+				productVersion, version, resources);
+			var stdOutput = string.Empty;
+			var exitCode = RunProcess(fileName, arguments, out stdOutput);
+			if (exitCode != 0)
 			{
-				process.StartInfo.UseShellExecute = false;
-				process.StartInfo.RedirectStandardOutput = true;
-				process.StartInfo.FileName = IsUnix ? "al" : "al.exe";
-				process.StartInfo.Arguments = BuildLinkerArgs(outputDllPath, culture, fileversion, productVersion, version, resources);
-				process.Start();
+				throw new ApplicationException(
+					$"Assembly linker returned error {exitCode} for {outputDllPath}.\n" +
+					$"Command line: {fileName} {arguments}\nOutput:\n{stdOutput}");
+			}
+		}
 
-				var stdOutput = process.StandardOutput.ReadToEnd();
-				process.WaitForExit();
-				if (process.ExitCode != 0)
+		private static int RunProcess(string fileName, string arguments, out string stdOutput,
+			int timeout = 300000 /* 5 min */)
+		{
+			var output = string.Empty;
+			using (var outputWaitHandle = new AutoResetEvent(false))
+			{
+				using (var process = new Process())
 				{
-					throw new ApplicationException($"Assembly linker returned error {process.ExitCode} for {outputDllPath}.\n" +
-						$"Command line: {process.StartInfo.FileName} {process.StartInfo.Arguments}\nOutput:\n{stdOutput}");
+					try
+					{
+						process.StartInfo.UseShellExecute = false;
+						process.StartInfo.RedirectStandardOutput = true;
+						process.StartInfo.FileName = fileName;
+						process.StartInfo.Arguments = arguments;
+						process.OutputDataReceived += (sender, e) =>
+						{
+							if (e.Data == null)
+								outputWaitHandle.Set();
+							else
+								output = e.Data;
+						};
+						process.Start();
+
+						process.BeginOutputReadLine();
+						process.WaitForExit(timeout);
+						stdOutput = output;
+						return process.ExitCode;
+					}
+					finally
+					{
+						outputWaitHandle.WaitOne(timeout);
+					}
 				}
 			}
 		}
@@ -244,35 +276,23 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 		protected virtual void RunResGen(string outputResourcePath, string localizedResxPath,
 			string originalResxFolder)
 		{
-			using (var process = new Process())
+			var fileName = IsUnix ? "resgen" : "resgen.exe";
+			var arguments = $"\"{localizedResxPath}\" \"{outputResourcePath}\"";
+			if (!IsUnix)
 			{
-				process.StartInfo.UseShellExecute = false;
-				process.StartInfo.RedirectStandardOutput = true;
-				process.StartInfo.FileName = IsUnix ? "resgen" : "resgen.exe";
-				process.StartInfo.Arguments = $"\"{localizedResxPath}\" \"{outputResourcePath}\"";
-				if (!IsUnix)
-				{
-					// It needs to be able to reference the appropriate System.Drawing.dll and System.Windows.Forms.dll to make the conversion.
-					var clrFolder = RuntimeEnvironment.GetRuntimeDirectory();
-					var drawingPath = Path.Combine(clrFolder, "System.Drawing.dll");
-					var formsPath = Path.Combine(clrFolder, "System.Windows.Forms.dll");
+				// It needs to be able to reference the appropriate System.Drawing.dll and System.Windows.Forms.dll to make the conversion.
+				var clrFolder = RuntimeEnvironment.GetRuntimeDirectory();
+				var drawingPath = Path.Combine(clrFolder, "System.Drawing.dll");
+				var formsPath = Path.Combine(clrFolder, "System.Windows.Forms.dll");
 
-					process.StartInfo.Arguments += $" /r:\"{drawingPath}\" /r:\"{formsPath}\"";
-				}
-
-				// Setting the working directory to the folder containing the ORIGINAL resx file allows us to find included files
-				// like FDO/Resources/Question.ico that the resx file refers to using relative paths.
-				process.StartInfo.WorkingDirectory = originalResxFolder;
-				process.Start();
-
-				var stdOutput = process.StandardOutput.ReadToEnd();
-				process.WaitForExit();
-
-				if (process.ExitCode != 0)
-				{
-					throw new ApplicationException($"Resgen returned error {process.ExitCode} for {localizedResxPath}.\n" +
-						$"Command line: {process.StartInfo.FileName} {process.StartInfo.Arguments}\nOutput:\n{stdOutput}");
-				}
+				arguments += $" /r:\"{drawingPath}\" /r:\"{formsPath}\"";
+			}
+			var stdOutput = string.Empty;
+			var exitCode = RunProcess(fileName, arguments, out stdOutput);
+			if (exitCode != 0)
+			{
+				throw new ApplicationException($"Resgen returned error {exitCode} for {localizedResxPath}.\n" +
+					$"Command line: {fileName} {arguments}\nOutput:\n{stdOutput}");
 			}
 		}
 

Build/Src/FwBuildTasks/Localization/Localizer.cs
@@ -94,14 +94,12 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 				if (string.IsNullOrEmpty(Version))
 					Version = FileVersion;
 
-				//Parallel.ForEach(projectFolders, currentFolder =>
-				foreach (var currentFolder in projectFolders)
+				Parallel.ForEach(projectFolders, currentFolder =>
 				{
 					var projectLocalizer = CreateProjectLocalizer(currentFolder,
 						new ProjectLocalizerOptions(this, Options));
 					projectLocalizer.ProcessProject();
-				}
-				//});
+				});
 			}
 			catch (Exception ex)
 			{

commit 307270de5378647a8d1f806fbbefb147e570dd21

Set working directory when running resgen
This fixes the package build which fd6744f997 broke.

Change-Id: Ifb67fda139a9c08143d69a0915f718152b70ea5f

Build/Src/FwBuildTasks/Localization/ProjectLocalizer.cs
@@ -213,7 +213,7 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 		}
 
 		private static int RunProcess(string fileName, string arguments, out string stdOutput,
-			int timeout = 300000 /* 5 min */)
+			int timeout = 300000 /* 5 min */, string workdir = null)
 		{
 			var output = string.Empty;
 			using (var outputWaitHandle = new AutoResetEvent(false))
@@ -226,6 +226,8 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 						process.StartInfo.RedirectStandardOutput = true;
 						process.StartInfo.FileName = fileName;
 						process.StartInfo.Arguments = arguments;
+						if (workdir != null)
+							process.StartInfo.WorkingDirectory = workdir;
 						process.OutputDataReceived += (sender, e) =>
 						{
 							if (e.Data == null)
@@ -288,7 +290,9 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 				arguments += $" /r:\"{drawingPath}\" /r:\"{formsPath}\"";
 			}
 			var stdOutput = string.Empty;
-			var exitCode = RunProcess(fileName, arguments, out stdOutput);
+			// Setting the working directory to the folder containing the ORIGINAL resx file allows us to find included files
+			// like FDO/Resources/Question.ico that the resx file refers to using relative paths.
+			var exitCode = RunProcess(fileName, arguments, out stdOutput, workdir: originalResxFolder);
 			if (exitCode != 0)
 			{
 				throw new ApplicationException($"Resgen returned error {exitCode} for {localizedResxPath}.\n" +