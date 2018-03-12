8871855bfd0348b797db36bd9fc6e132f3cdeb11
Hasso Pape <hasso_pape@sil.org>
2018-03-12 14:26:48 -0500

    LT-18934: build 64-bit localization
    Include a path to resgen.exe in the PATH
    
    Change-Id: I9324d0fe8c2024f9387dd1d421dfc079e5c1c0ac

--- Build/build64.bat
+++ Build/build64.bat
@@ -2,5 +2,7 @@ setlocal
 
 REM set an environment variable that we use in build.bat to set the correct visual studio environment
 set arch=x64
+REM the corresponding x64 SDK Tools directory does not provide resgen.exe, which is needed for localization.
+set PATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools;%PATH%
 REM call build.bat passing it the x64 platform
 build.bat /p:Platform=x64 %*
