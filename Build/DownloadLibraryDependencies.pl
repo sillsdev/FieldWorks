4fb62b4d4e14fb63ebfc10eb852669e22d0f430d
Raymond Luong <raymond_luong@sil.org>
2018-04-20 13:26:37 -0500

    LT-19012 Add target to build FW required libraries locally
    * Added a perl script to download library dependencies
    
    Change-Id: Ic9a9971931b84a76c4b8fb0578c8284142c16120

---
+++ Build/DownloadLibraryDependencies.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+# This script is used to download the dependencies to build libpalaso, liblcm and chorus
+# libraries locally for debugging FLEx. It find the locations to these local repos by parsing
+# the LibraryDevelopment.properties file.
+
+# Run: perl DownloadLibraryDependencies.pl
+# Note: you will need to install XML::Simple to run this script.
+# XML::Simple is included in Strawberry Perl on windows (http://strawberryperl.com/)
+# XML::Simple installation can be done on linux using the CPAN shell:
+#   shell> perl -MCPAN -e shell
+#   cpan> install XML::Simple
+
+# After this script terminates, do the following to build libraries locally
+#   Windows: build /t:LocalLibrary /p:Platform=<x86|x64>
+#   Linux: msbuild /t:LocalLibrary
+use XML::Simple;
+
+my $osName = "$^O";
+my $extension = "win.sh";
+if ($osName eq "linux") {
+	$extension = "mono.sh";
+}
+my $xml = new XML::Simple;
+my $data = $xml->XMLin("LibraryDevelopment.properties");
+
+my $palasoArtifactsDir = "$data->{PropertyGroup}->{'PalasoArtifactsDir'}->{content}";
+my $chorusArtifactsDir = "$data->{PropertyGroup}->{'ChorusArtifactsDir'}->{content}";
+print "Downloading libpalaso dependencies...\n";
+
+# Look for "output" in the path and take everything before it
+$palasoArtifactsDir =~ m/output/;
+my $palasoBase = "$`";
+# Replace backslashes with forward slashes
+$palasoBase =~ s/\\/\//g;
+system($palasoBase . "build/buildupdate.$extension");
+print "Finished downloading libpalaso dependencies.\n";
+print "Downloading chorus dependencies...\n";
+$chorusArtifactsDir =~ m/output/;
+my $chorusBase = "$`";
+$chorusBase =~ s/\\/\//g;
+system($chorusBase . "build/buildupdate.$extension");
+print "Finished downloading chorus dependencies.\n";
