970153c2da721e663807b4951233615dbf9aa2d7
MarkS <marksvc@gmail.com>
2018-07-06 15:49:01 -0500

    Set global FIELDWORKSDIR environment variable
    * To help ParaText find FieldWorks.
    * Implements LT-19200.
    
    Change-Id: Iec3118acc0acdfda1576cfcae923a11bfd4667ff

---
+++ Lib/linux/fieldworks.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2018 SIL International
+# This software is licensed under the LGPL, version 2.1 or later
+# (http://www.gnu.org/licenses/lgpl-2.1.html)
+#
+# /etc/profile.d/fieldworks.sh
+#
+# Help other programs find FieldWorks.
+
+FIELDWORKSDIR=/usr/lib/fieldworks
+export FIELDWORKSDIR
