812b1c8497cef2ef91c0b734321826909a4d83ac
Randy Regnier <rbregnier@gmail.com>
2018-12-06 08:15:11 -0700

    LT-19423 - Clean up code in ManagedVwWindow assembly and its test assembly
    Change-Id: Id3866c45c97ca2c9e6e4a2aa213857970d161b1e

--- Src/ManagedVwWindow/ManagedVwWindow.cs
+++ Src/ManagedVwWindow/ManagedVwWindow.cs
@@ -1,30 +1,32 @@
-// Copyright (c) 2015 SIL International
+// Copyright (c) 2010-2018 SIL International
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using SIL.FieldWorks.Common.ViewsInterfaces;
-using System.Runtime.InteropServices;
 
 namespace SIL.FieldWorks.Views
 {
 	/// <summary>
-	/// This class wrapps a hwnd to allow cross platform access to
+	/// This class wraps a hwnd to allow cross platform access to
 	/// window properties.
 	/// </summary>
 	[Guid("3fb0fcd2-ac55-42a8-b580-73b89a2b6215")]
 	public class ManagedVwWindow : IVwWindow
 	{
 		// the wrapped Window
-		protected Control m_control = null;
+		protected Control m_control;
 
 		#region IVwWindow Members
 
 		public void GetClientRectangle(out Rect clientRectangle)
 		{
-			if ( m_control == null)
+			if (m_control == null)
+			{
 				throw new ApplicationException("Window not set");
+			}
 
 			clientRectangle.top = m_control.ClientRectangle.Top;
 			clientRectangle.left = m_control.ClientRectangle.Left;
@@ -36,12 +38,11 @@ namespace SIL.FieldWorks.Views
 		{
 			set
 			{
-				var ptr = (IntPtr) value;
+				var ptr = (IntPtr)value;
 				m_control = Control.FromHandle(ptr);
 			}
 		}
 
 		#endregion
 	}
-
-} // end namespace SIL.FieldWorks.Views
+}
