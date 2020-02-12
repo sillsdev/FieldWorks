b777fb51da5da77b1a6e78abaef545cc977ce2f4
Hasso <hasso_pape@sil.org>
2020-02-12 13:59:54 -0600

    LT-20025: Use Crowdin's translations for lists
    Change-Id: If951de8535f417e11b8243b5963c1373549a0cb3

---
+++ Build/Src/FwBuildTasks/Localization/ListsToXliff.cs
@@ -0,0 +1,30 @@
+// Copyright (c) 2020 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace SIL.FieldWorks.Build.Tasks.Localization
+{
+	/// <summary>
+	/// Convert FieldWorks lists from XML to XLIFF and split into several files to facilitate localization.
+	/// </summary>
+	public class ListsToXliff : Task
+	{
+		/// <summary>The XML file containing all FieldWorks list data</summary>
+		[Required]
+		public string SourceXml { get; set; }
+
+		/// <summary>The directory where XLIFF files should be saved</summary>
+		[Required]
+		public string XliffOutputDir { get; set; }
+
+		public override bool Execute()
+		{
+			Log.LogMessage($"Converting '{SourceXml}' to XLIFF at '{XliffOutputDir}'.");
+			LocalizeLists.SplitSourceLists(SourceXml, XliffOutputDir);
+			return true;
+		}
+	}
+}
