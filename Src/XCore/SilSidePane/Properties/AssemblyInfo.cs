d217b9147bfbe17be41a93ea131184d0dfd25674
MarkS <marksvc@gmail.com>
2021-04-06 17:36:50 -0500

    Update SilSidePane license
    Change-Id: Ia1670d938740f0f175f56d1da540aa731948a1c6

--- Src/XCore/SilSidePane/Properties/AssemblyInfo.cs
+++ Src/XCore/SilSidePane/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
-﻿// SilSidePane, Copyright 2009 SIL International. All rights reserved.
-// SilSidePane is licensed under the Code Project Open License (CPOL), <http://www.codeproject.com/info/cpol10.aspx>.
-// Derived from OutlookBar v2 2005 <http://www.codeproject.com/KB/vb/OutlookBar.aspx>, Copyright 2007 by Star Vega.
-// Changed in 2008 and 2009 by SIL International to convert to C# and add more functionality.
+﻿// Copyright (c) 2016 SIL International
+// SilOutlookBar is licensed under the MIT license.
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using SIL.Acknowledgements;
 
 [assembly: AssemblyTitle("SilSidePane")]
 
@@ -13,3 +12,12 @@ using System.Runtime.InteropServices;
 
 // Expose IItemArea to unit tests
 [assembly: InternalsVisibleTo("SilSidePaneTests")]
+
+// SilSidePane is derived from OutlookBar <http://www.codeproject.com/KB/vb/OutlookBar.aspx>, by Star Vega.
+// It was changed in 2008 and 2009 by SIL International to convert to C# and add more functionality.
+[assembly: Acknowledgement("SilSidePane",
+	Name = "SilSidePane",
+	Copyright = "Copyright (C) 2008-$YEAR SIL International. Copyright (C) 2007 Star Vega. Licensed under the MIT license.",
+	Url = "https://github.com/sillsdev/FieldWorks/tree/develop/Src/XCore/SilSidePane",
+	LicenseUrl = "https://opensource.org/licenses/MIT",
+	Location = "./SilSidePane.dll")]
