71c193d789abd3584c8f290c44a83b13945669e4
Mark Kidder <mark_kidder@sil.org>
2022-10-12 11:21:39 -0400

    LT-21150: Merge Src/FwResources
    Change-Id: I14bb07ad455bda1146853e8c44a8471faf23716f

--- Src/FwResources/ResourceHelper.cs
+++ Src/FwResources/ResourceHelper.cs
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-// Copyright (c) 2004-2020 SIL International
-||||||| f013144d5
-﻿// Copyright (c) 2004-2015 SIL International
-=======
-// Copyright (c) 2004-2021 SIL International
->>>>>>> develop
+// Copyright (c) 2004-2022 SIL International
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
@@ -100,16 +94,6 @@ namespace SIL.FieldWorks.Resources
 
 		internal static ResourceHelperImpl Helper
 		{
-<<<<<<< HEAD
-			get => s_form ?? (s_form = new ResourceHelperImpl());
-||||||| f013144d5
-			get
-			{
-				if (s_form == null)
-					s_form = new ResourceHelperImpl();
-				return s_form;
-			}
-=======
 			get
 			{
 				var sForm = SingletonsContainer.Get<ResourceHelperImpl>();
@@ -121,17 +105,8 @@ namespace SIL.FieldWorks.Resources
 
 				return sForm;
 			}
->>>>>>> develop
 			set
 			{
-<<<<<<< HEAD
-				s_form?.Dispose();
-				s_form = value;
-||||||| f013144d5
-				if (s_form != null)
-					s_form.Dispose();
-				s_form = value;
-=======
 				var sForm = SingletonsContainer.Get<ResourceHelperImpl>();
 				if (sForm != null)
 				{
@@ -140,7 +115,6 @@ namespace SIL.FieldWorks.Resources
 				}
 				sForm = value;
 				SingletonsContainer.Add(sForm);
->>>>>>> develop
 			}
 		}
 
