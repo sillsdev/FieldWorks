36465d562ca3c3e5d1d5e972b63daab6f4acf58c
Hasso <hasso_pape@sil.org>
2022-10-24 15:15:04 -0500

    Fix some build errors, clean up some references
    Remove extra FwBuildTasks files
    that are needed only for the deprecated Nuget package.
    
    Change-Id: Id63fedd1f99a29418f47552652f5a651b4490afb

--- Build/Src/FwBuildTasks/SIL.FwBuildTasks/Localization/ProjectLocalizerOptions.cs
+++ Build/Src/FwBuildTasks/SIL.FwBuildTasks/Localization/ProjectLocalizerOptions.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2020 SIL International
+// Copyright (c) 2015-2022 SIL International
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
@@ -21,7 +21,7 @@ namespace SIL.FieldWorks.Build.Tasks.Localization
 
 		public string Version { get; }
 		public string FileVersion { get; }
-		public string InformationVersion { get; }
+		public new string InformationVersion { get; }
 		public string Locale { get; }
 		public string CurrentLocaleDir { get; }
 
