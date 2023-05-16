sillsdev/FieldWorks

commit 5d822c97e981926c1889d93ec6e2ab7a03a4285f

Fix for LT-18222 - ws option order not maintained
* Retain the user order for writing system options

Change-Id: I3e19f0f83291bacf220f582dda510a48f936cd03

Src/xWorks/DictionaryConfigurationController.cs
@@ -1070,23 +1070,14 @@ namespace SIL.FieldWorks.XWorks
 		public static List<DictionaryNodeListOptions.DictionaryNodeOption> UpdateWsOptions(DictionaryNodeWritingSystemOptions wsOptions, FdoCache cache)
 		{
 			var availableWSs = GetCurrentWritingSystems(wsOptions.WsType, cache);
-
-			if (wsOptions.Options.Count == availableWSs.Count)
+			int magicId;
+			wsOptions.Options.AddRange(availableWSs.Where(availWs => !int.TryParse(availWs.Id, out magicId) && wsOptions.Options.All(opt => opt.Id != availWs.Id)));
+			for (var i = wsOptions.Options.Count - 1; i >= 0; --i)
 			{
-				int i = 0;
-				foreach (var ws in availableWSs)
+				if (availableWSs.All(opt => opt.Id != wsOptions.Options[i].Id) &&
+					WritingSystemServices.GetMagicWsIdFromName(wsOptions.Options[i].Id) == 0)
 				{
-					int magicId;
-					if (int.TryParse(ws.Id, out magicId))
-					{
-						var wsName = WritingSystemServices.GetMagicWsNameFromId(magicId);
-						wsOptions.Options[i].Id = wsName;
-					}
-					else
-					{
-						wsOptions.Options[i].Id = ws.Id;
-					}
-					i = i + 1;
+					wsOptions.Options.RemoveAt(i);
 				}
 			}
 			return availableWSs;

Src/xWorks/xWorksTests/DictionaryConfigurationControllerTests.cs
@@ -1006,6 +1006,52 @@ namespace SIL.FieldWorks.XWorks
 		}
 
 		[Test]
+		public void UpdateWsOptions_OrderAndCheckMaintained()
+		{
+			IWritingSystem wsEs;
+			Cache.ServiceLocator.WritingSystemManager.GetOrSet("es", out wsEs);
+			Cache.ServiceLocator.WritingSystems.AddToCurrentVernacularWritingSystems(wsEs);
+			var model = new DictionaryConfigurationModel();
+			var customNode = new ConfigurableDictionaryNode()
+			{
+				Label = "CustomString",
+				FieldDescription = "CustomString",
+				IsCustomField = true,
+				DictionaryNodeOptions = new DictionaryNodeWritingSystemOptions
+				{
+					DisplayWritingSystemAbbreviations = true,
+					Options = new List<DictionaryNodeListOptions.DictionaryNodeOption>
+					{
+						new DictionaryNodeListOptions.DictionaryNodeOption() { Id = "ch", IsEnabled = true },
+						new DictionaryNodeListOptions.DictionaryNodeOption() { Id = "fr", IsEnabled = true },
+						new DictionaryNodeListOptions.DictionaryNodeOption() { Id = "en", IsEnabled = true }
+					},
+					WsType = DictionaryNodeWritingSystemOptions.WritingSystemType.Both
+				}
+			};
+			var entryNode = new ConfigurableDictionaryNode
+			{
+				Label = "Main Entry",
+				FieldDescription = "LexEntry",
+				Children = new List<ConfigurableDictionaryNode> { customNode }
+			};
+			model.Parts = new List<ConfigurableDictionaryNode> { entryNode };
+			CssGeneratorTests.PopulateFieldsForTesting(model);
+
+			//SUT
+			var updatedList = DictionaryConfigurationController.UpdateWsOptions((DictionaryNodeWritingSystemOptions)customNode.DictionaryNodeOptions, Cache);
+			Assert.AreEqual(1, model.Parts[0].Children.Count, "Only the existing custom field node should be present");
+			var wsOptions = model.Parts[0].Children[0].DictionaryNodeOptions as DictionaryNodeWritingSystemOptions;
+			Assert.NotNull(wsOptions, "Writing system options lost in merge");
+			Assert.IsTrue(wsOptions.DisplayWritingSystemAbbreviations, "WsAbbreviation lost in merge");
+			Assert.AreEqual("fr", wsOptions.Options[0].Id, "Writing system not removed, or order not maintained");
+			Assert.IsTrue(wsOptions.Options[0].IsEnabled, "Selected writing system lost in merge");
+			Assert.AreEqual("en", wsOptions.Options[1].Id);
+			Assert.IsTrue(wsOptions.Options[1].IsEnabled, "Selected writing system lost in merge");
+			Assert.AreEqual("es", wsOptions.Options[2].Id, "New writing system was not added");
+		}
+
+		[Test]
 		public void MergeCustomFieldsIntoDictionaryModel_NewFieldsOnSharedNodesAreAddedToSharedItemsExclusively()
 		{
 			var subSubsNode = new ConfigurableDictionaryNode { Label = "Subsubs", FieldDescription = "Subentries", ReferenceItem = "SharedSubs" };

Src/xWorks/xWorksTests/DictionaryConfigurationModelTests.cs
@@ -118,7 +118,7 @@ namespace SIL.FieldWorks.XWorks
 			using (var modelFile = new TempFile(new[]
 			{
 				XmlOpenTagsThruHeadword, @"
-				<WritingSystemOptions writingSystemType=""analysis"" displayWSAbreviation=""true"">
+				<WritingSystemOptions writingSystemType=""vernacular"" displayWSAbreviation=""true"">
 					<Option id=""fr"" isEnabled=""true""/>
 				</WritingSystemOptions>",
 				XmlCloseTagsFromHeadword
@@ -132,7 +132,7 @@ namespace SIL.FieldWorks.XWorks
 			Assert.IsInstanceOf(typeof(DictionaryNodeWritingSystemOptions), testNodeOptions);
 			var wsOptions = (DictionaryNodeWritingSystemOptions)testNodeOptions;
 			Assert.IsTrue(wsOptions.DisplayWritingSystemAbbreviations);
-			Assert.AreEqual(DictionaryNodeWritingSystemOptions.WritingSystemType.Analysis, wsOptions.WsType);
+			Assert.AreEqual(DictionaryNodeWritingSystemOptions.WritingSystemType.Vernacular, wsOptions.WsType);
 			Assert.AreEqual(1, wsOptions.Options.Count);
 			Assert.AreEqual("fr", wsOptions.Options[0].Id);
 			Assert.IsTrue(wsOptions.Options[0].IsEnabled);

commit fb4e8170e498ad84ceb23f214b97073496ac4676

LT-21308: Display enabled writing systems
Do not hide writing systems that are enabled.  Also changed
a tooltip.

Notes for future development:
In Feb 2017 this code was originally added with Commit 577e4293
and it looks like it would have displayed the enabled ws’s.
It appears to have regressed with Commit 5d822c97, a month and a
half later.

There are currently two situations where we do not preserve
the user specified order:
1. The order of the ws’s that are not enabled is currently
not preserved.
2. Instead of preserving the user order we group the
enabled ws’s to all be immediately after the default(s).
The code in UpdateWsOptions() and LoadAvailableWsList() could
be cleaned up if we stopped supporting these behaviors (it could
also be cleaned up to a lesser extent if we continued to support
them).

Change-Id: I87466c0633fdd28125dca1d4ea4e79238be299f3

Src/FwCoreDlgs/FwCoreDlgs.Designer.cs
@@ -2406,7 +2406,7 @@ namespace SIL.FieldWorks.FwCoreDlgs {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Writing systems with selected checkboxes are always displayed..
+        ///   Looks up a localized string similar to Writing systems with cleared checkboxes are displayed only for fields that are not empty..
         /// </summary>
         public static string WritingSystemList_NormalTooltip {
             get {

Src/FwCoreDlgs/FwCoreDlgs.resx
@@ -985,7 +985,7 @@ Delete the existing relationship before trying to add this one again.</value>
     <comment>Message and tooltip shown when the user has cleared all writing system checkboxes and clicked OK</comment>
   </data>
   <data name="WritingSystemList_NormalTooltip" xml:space="preserve">
-    <value>Writing systems with selected checkboxes are always displayed.</value>
+    <value>Writing systems with cleared checkboxes are displayed only for fields that are not empty.</value>
     <comment>Tooltip for the writing system list in the writing system setup dialog</comment>
   </data>
   <data name="WritingSystemList_AddIpa" xml:space="preserve">

Src/xWorks/DictionaryConfigurationController.cs
@@ -1145,7 +1145,16 @@ namespace SIL.FieldWorks.XWorks
 				if (availableWSs.All(opt => opt.Id != wsOptions.Options[i].Id) &&
 					WritingSystemServices.GetMagicWsIdFromName(wsOptions.Options[i].Id) == 0)
 				{
-					wsOptions.Options.RemoveAt(i);
+					// If Enabled, then add it to availableWSs.
+					if (wsOptions.Options[i].IsEnabled)
+					{
+						availableWSs.Add(wsOptions.Options[i]);
+					}
+					// If not Enabled, then remove it from wsOptions.Options.
+					else
+					{
+						wsOptions.Options.RemoveAt(i);
+					}
 				}
 			}
 			// ensure at least one is enabled (default to the first, which is always Magic)

Src/xWorks/xWorksTests/DictionaryConfigurationControllerTests.cs
@@ -1040,8 +1040,9 @@ namespace SIL.FieldWorks.XWorks
 				FieldDescription = "CustomString",
 				IsCustomField = true,
 				DictionaryNodeOptions = ConfiguredXHTMLGeneratorTests.GetWsOptionsForLanguageswithDisplayWsAbbrev(
-					new[] { "ch", "fr", "en" }, DictionaryNodeWritingSystemOptions.WritingSystemType.Both)
+					new[] { "ch", "fr", "en", "ru" }, DictionaryNodeWritingSystemOptions.WritingSystemType.Both)
 			};
+
 			var entryNode = new ConfigurableDictionaryNode
 			{
 				Label = "Main Entry",
@@ -1050,9 +1051,10 @@ namespace SIL.FieldWorks.XWorks
 			};
 			model.Parts = new List<ConfigurableDictionaryNode> { entryNode };
 			CssGeneratorTests.PopulateFieldsForTesting(model);
+			(customNode.DictionaryNodeOptions as DictionaryNodeWritingSystemOptions).Options[0].IsEnabled = false;
 
 			//SUT
-			DictionaryConfigurationController.UpdateWsOptions((DictionaryNodeWritingSystemOptions)customNode.DictionaryNodeOptions, Cache);
+			var availableWSs = DictionaryConfigurationController.UpdateWsOptions((DictionaryNodeWritingSystemOptions)customNode.DictionaryNodeOptions, Cache);
 			Assert.AreEqual(1, model.Parts[0].Children.Count, "Only the existing custom field node should be present");
 			var wsOptions = model.Parts[0].Children[0].DictionaryNodeOptions as DictionaryNodeWritingSystemOptions;
 			Assert.NotNull(wsOptions, "Writing system options lost in merge");
@@ -1061,7 +1063,19 @@ namespace SIL.FieldWorks.XWorks
 			Assert.IsTrue(wsOptions.Options[0].IsEnabled, "Selected writing system lost in merge");
 			Assert.AreEqual("en", wsOptions.Options[1].Id);
 			Assert.IsTrue(wsOptions.Options[1].IsEnabled, "Selected writing system lost in merge");
-			Assert.AreEqual("es", wsOptions.Options[2].Id, "New writing system was not added");
+			Assert.AreEqual("ru", wsOptions.Options[2].Id, "Enabled writing system was not maintained");
+			Assert.IsTrue(wsOptions.Options[2].IsEnabled, "Selected writing system lost in merge");
+			Assert.AreEqual("es", wsOptions.Options[3].Id, "New writing system was not added");
+
+			// Check availableWSs
+			Assert.IsTrue(availableWSs.Count == 6);
+			List<string> availableWSsIds = availableWSs.Select(ws => ws.Id).ToList();
+			Assert.Contains("-2", availableWSsIds);
+			Assert.Contains("-1", availableWSsIds);
+			Assert.Contains("fr", availableWSsIds);
+			Assert.Contains("es", availableWSsIds);
+			Assert.Contains("en", availableWSsIds);
+			Assert.Contains("ru", availableWSsIds);
 		}
 
 		[Test]