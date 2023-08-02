sillsdev/FieldWorks

commit eeb9f0993da2a89edfc0d9e08aefb1cbe03a35ee

LT-21403: Implement WritingSystemHvo property change
Implement the WritingSystemHvo property change in the Pub/Sub
System.

Change-Id: I974283f02c00aec1f16bdf5fe434b2b38660cfdf

Src/Common/SimpleRootSite/SimpleRootSite.cs
@@ -412,6 +412,8 @@ namespace SIL.FieldWorks.Common.RootSites
 
 			if (disposing)
 			{
+				FwUtils.FwUtils.Subscriber.Unsubscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
+
 				// Do this here, before disposing m_messageSequencer,
 				// as we still get messages during dispose.
 				// Once the the base class has shut down the window handle,
@@ -2074,10 +2076,6 @@ namespace SIL.FieldWorks.Common.RootSites
 			CheckDisposed();
 			switch (name)
 			{
-				case "WritingSystemHvo":
-					EditingHelper.WritingSystemHvoChanged();
-					break;
-
 				case "BestStyleName":
 					EditingHelper.BestStyleNameChanged();
 					break;
@@ -2085,6 +2083,21 @@ namespace SIL.FieldWorks.Common.RootSites
 				default:
 					break;
 			}
+		}
+
+		/// <summary>
+		/// Handle "WritingSystemHvo" message.
+		/// </summary>
+		protected virtual void WritingSystemHvo_Changed(object newValue)
+		{
+			CheckDisposed();
+			if (!Focused)
+			{
+				FwUtils.FwUtils.Subscriber.Unsubscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
+				return;
+			}
+
+			EditingHelper.WritingSystemHvoChanged();
 		}
 
 		/// ------------------------------------------------------------------------------------
@@ -3341,6 +3354,15 @@ namespace SIL.FieldWorks.Common.RootSites
 			Activate(VwSelectionState.vssEnabled);
 
 			EditingHelper.GotFocus();
+
+			FwUtils.FwUtils.Subscriber.Subscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
+		}
+
+		/// <summary />
+		protected override void OnLostFocus(EventArgs e)
+		{
+			FwUtils.FwUtils.Subscriber.Unsubscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
+			base.OnLostFocus(e);
 		}
 
 		/// ------------------------------------------------------------------------------------

Src/LexText/Interlinear/RawTextPane.cs
@@ -288,37 +288,10 @@ namespace SIL.FieldWorks.IText
 		/// </summary>
 		public override void OnPropertyChanged(string name)
 		{
-			var wsBefore = 0;
-			// We want to know below whether a base class changed the ws or not.
-			if (name == "WritingSystemHvo")
-			{
-				if (RootObject != null && m_rootb != null && m_rootb.Selection.IsValid)
-					wsBefore = SelectionHelper.GetWsOfEntireSelection(m_rootb.Selection);
-			}
-
 			base.OnPropertyChanged(name);
 			bool newVal; // used in two cases below
 			switch (name)
 			{
-				case "WritingSystemHvo":
-					if (RootObject != null && m_rootb != null && m_rootb.Selection.IsValid)
-					{
-						int hvo, tag, ws, ichMin, ichLim;
-						ws = SelectionHelper.GetWsOfEntireSelection(m_rootb.Selection);
-						if (ws != wsBefore) // writing system changed!
-						{
-							if (GetSelectedWordPos(m_rootb.Selection, out hvo, out tag, out ws, out ichMin, out ichLim))
-							{
-								if (tag != StTxtParaTags.kflidContents)
-									return;
-
-								var para = m_cache.ServiceLocator.GetInstance<IStTxtParaRepository>().GetObject(hvo);
-								// force this paragraph to recognize it might need reparsing.
-								SetParaToReparse(para);
-							}
-						}
-					}
-					break;
 				case "ShowInvisibleSpaces":
 					newVal = ShowInvisibleSpaces;
 					if (newVal != m_showSpaceDa.ShowSpaces)
@@ -341,6 +314,37 @@ namespace SIL.FieldWorks.IText
 					break;
 				default:
 					break;
+			}
+		}
+
+		/// <summary>
+		/// Handle "WritingSystemHvo" message.
+		/// </summary>
+		protected override void WritingSystemHvo_Changed(object newValue)
+		{
+			var wsBefore = 0;
+			// We want to know below whether a base class changed the ws or not.
+			if (RootObject != null && m_rootb != null && m_rootb.Selection.IsValid)
+				wsBefore = SelectionHelper.GetWsOfEntireSelection(m_rootb.Selection);
+
+			base.WritingSystemHvo_Changed(newValue);
+
+			if (RootObject != null && m_rootb != null && m_rootb.Selection.IsValid)
+			{
+				int hvo, tag, ws, ichMin, ichLim;
+				ws = SelectionHelper.GetWsOfEntireSelection(m_rootb.Selection);
+				if (ws != wsBefore) // writing system changed!
+				{
+					if (GetSelectedWordPos(m_rootb.Selection, out hvo, out tag, out ws, out ichMin, out ichLim))
+					{
+						if (tag != StTxtParaTags.kflidContents)
+							return;
+
+						var para = m_cache.ServiceLocator.GetInstance<IStTxtParaRepository>().GetObject(hvo);
+						// force this paragraph to recognize it might need reparsing.
+						SetParaToReparse(para);
+					}
+				}
 			}
 		}
 

Src/XCore/xCoreInterfaces/PropertyTable.cs
@@ -562,7 +562,7 @@ namespace XCore
 
 			if (didChange && doBroadcastIfChanged)
 			{
-				BroadcastPropertyChange(key);
+				BroadcastPropertyChange(key, newValue);
 			}
 
 #if SHOWTRACE
@@ -573,10 +573,15 @@ namespace XCore
 #endif
 		}
 
-		private void BroadcastPropertyChange(string key)
+		private void BroadcastPropertyChange(string key, object newValue)
 		{
 			var localSettingsPrefix = GetPathPrefixForSettingsId(LocalSettingsId);
 			var propertyName = key.StartsWith(localSettingsPrefix) ? key.Remove(0, localSettingsPrefix.Length) : key;
+
+			if (!string.IsNullOrWhiteSpace(propertyName))
+			{
+				FwUtils.Publisher.Publish(new PublisherParameterObject(propertyName, newValue));
+			}
 
 			if (Mediator != null)
 			{

Src/XCore/xWindow.cs
@@ -441,6 +441,8 @@ namespace XCore
 			m_mediator.SpecificToOneMainWindow = true;
 
 			InitializeComponent();
+
+			FwUtils.Subscriber.Subscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
 		}
 
 		/// <summary>
@@ -1318,6 +1320,7 @@ namespace XCore
 		/// </summary>
 		private void ShutDownPart1()
 		{
+			FwUtils.Subscriber.Unsubscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
 
 			if (m_mediator != null)
 			{
@@ -1903,7 +1906,6 @@ namespace XCore
 					break;
 
 				// Refresh the Writing System and Styles Combo Boxes
-				case "WritingSystemHvo":
 				case "BestStyleName":
 					SynchronizedOnIdleTime();
 					break;
@@ -1920,6 +1922,16 @@ namespace XCore
 					}
 					break;
 			}
+		}
+
+		/// <summary>
+		/// Handle "WritingSystemHvo" message.
+		/// </summary>
+		private void WritingSystemHvo_Changed(object newValue)
+		{
+			CheckDisposed();
+			// Refresh the Writing System and Styles Combo Boxes
+			SynchronizedOnIdleTime();
 		}
 
 		/// <summary>

commit 0ebacbb072f7b7427649d1b995dcead5dd4b55d7

LT-21568: Cleanup WritingSystemHvo property change
Did not add the cleanup to RawTextPane.cs because there are other
code paths in EditingHelper::WritingSystemHvoChanged() that could
results in returning without changing the writing system.

Note:
In addition to cleanup this change also fixed a bug that was
introduced in Commit eeb9f099. The defect was because
m_fSuppressNextWritingSystemHvoChanged was being set to true after
the SetProperty() call in EditingHelper.cs.  This was ok when we were
processing the property change OnIdle, but did not work when commit
eeb9f099 change the property change handling to be immediate.

Change-Id: I9be87f0af9955abac657f0b69227d9706665818b

Src/Common/SimpleRootSite/EditingHelper.cs
@@ -247,7 +247,6 @@ namespace SIL.FieldWorks.Common.RootSites
 		/// <summary>Event for changing properties of a pasted TsString</summary>
 		public event FwPasteFixTssEventHandler PasteFixTssEvent;
 
-		private bool m_fSuppressNextWritingSystemHvoChanged;
 		private long m_TimestampOfLastGotFocus;
 
 		/// <summary>Flag to prevent reentrancy while setting keyboard.</summary>
@@ -2839,19 +2838,21 @@ namespace SIL.FieldWorks.Common.RootSites
 				int oldWritingSystemHvo = int.Parse(s);
 				if (oldWritingSystemHvo != ws)
 				{
-					rs.PropTable.SetProperty("WritingSystemHvo", ws.ToString(), true);
-					rs.PropTable.SetPropertyPersistence("WritingSystemHvo", false);
-					m_fSuppressNextWritingSystemHvoChanged = true;
+					try
+					{
+						rs.UnsubscribeFromWritingSystemHvoChangedEvent();
+						rs.PropTable.SetProperty("WritingSystemHvo", ws.ToString(), true);
+						rs.PropTable.SetPropertyPersistence("WritingSystemHvo", false);
+					}
+					finally
+					{
+						rs.SubscribeToWritingSystemHvoChangedEvent();
+					}
 				}
 			}
 		}
 		internal void WritingSystemHvoChanged()
 		{
-			if (m_fSuppressNextWritingSystemHvoChanged)
-			{
-				m_fSuppressNextWritingSystemHvoChanged = false;
-				return;
-			}
 			// For now, we are only handling SimpleRootSite cases, e.g. for the Data Tree.
 			// If we need this in print layout, consider adding the mediator to the Callbacks
 			// interface.

Src/Common/SimpleRootSite/SimpleRootSite.cs
@@ -2075,6 +2075,24 @@ namespace SIL.FieldWorks.Common.RootSites
 			EditingHelper.WritingSystemHvoChanged();
 		}
 
+		/// <summary>
+		/// Allow other classes to temporarily unsubscribe from the
+		/// WritingSystemHvo_Changed event.
+		/// </summary>
+		public void UnsubscribeFromWritingSystemHvoChangedEvent()
+		{
+			FwUtils.FwUtils.Subscriber.Unsubscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
+		}
+
+		/// <summary>
+		/// Allow other classes to re-subscribe to the WritingSystemHvo_Changed
+		/// event (after temporarily unsubscribing).
+		/// </summary>
+		public void SubscribeToWritingSystemHvoChangedEvent()
+		{
+			FwUtils.FwUtils.Subscriber.Subscribe(PropertyConstants.WritingSystemHvo, WritingSystemHvo_Changed);
+		}
+
 		/// ------------------------------------------------------------------------------------
 		/// <summary>
 		/// Called (by xcore) to control display params of the writing system menu, e.g. whether