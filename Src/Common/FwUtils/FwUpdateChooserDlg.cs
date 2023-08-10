2b3fbe432f839108d8ee33dd2da2262ca69d9034
Hasso <hasso_pape@sil.org>
2023-08-10 10:22:33 -0500

    LT-21577: clarify installer metadata for testers
    FW doesn't know when FB was built. We could find out, but
    only testers see this information.
    
    Change-Id: Ib9615095305d6042404691adf588bda329f4bfbf

--- Src/Common/FwUtils/FwUpdateChooserDlg.cs
+++ Src/Common/FwUtils/FwUpdateChooserDlg.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2021 SIL International
+// Copyright (c) 2021-2023 SIL International
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
@@ -21,11 +21,12 @@ namespace SIL.FieldWorks.Common.FwUtils
 			// FwUpdate.ToString does not always include the base build number (which could be informative) but does include the installer type,
 			// even though we have no way of knowing what that was for the current version; define our own relevant parts here.
 			tbInstructions.Text =
-				$"The following installers are available. You currently have {current.Version}_b{current.BaseBuild} built {current.Date} installed. " +
-				"Additional patches may be available on the listed base (online and offline) installers. " +
-				"To download an update, select it and click Download; another dialog will appear when the download is complete. " +
-				$"To install base installers, you may need to uninstall FLEx and then install manually from {FwDirectoryFinder.DownloadedUpdates}.";
+				$"The following installers are available. You currently have {current.Version}_b{current.BaseBuild} built {current.Date:yyyy-MM-dd} installed" +
+				" (date is invalid for FLEx Bridge). Additional patches may be available on the listed base (online and offline) installers. " +
+				"To download an update, double-click, or select it and click Download; another dialog will appear when the download is complete. " +
+				$"To install FieldWorks base installers, you may need to uninstall FLEx and then install manually from {FwDirectoryFinder.DownloadedUpdates}.";
 
+			// ReSharper disable once CoVariantArrayConversion
 			lbChooseVersion.Items.AddRange(available.ToArray());
 		}
 
