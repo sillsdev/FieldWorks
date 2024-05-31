60f9aaae09a3c76a3054b140b15f9b9c4b6db182
John Maxwell <maxwellparc@gmail.com>
2024-05-31 08:31:20 -0700

    Make changes requested by Andy Black

--- Src/LexText/ParserUI/XAmpleTrace.cs
+++ Src/LexText/ParserUI/XAmpleTrace.cs
@@ -13,6 +13,7 @@
 //		XAmpleTrace - Deal with results of an XAmple trace
 // </remarks>
 
+using System.Linq;
 using System.Xml.Linq;
 using XCore;
 
@@ -65,8 +66,39 @@ namespace SIL.FieldWorks.LexText.Controls
 			{
 				transform = ParseTransform;
 				baseName = "XAmpleParse";
+				result = FixAnyDoubleQuotes(result);
 			}
 			return transform.Transform(propertyTable, result, baseName);
+		}
+
+		/// <summary>
+		/// Convert any &amp;quot; sequences in the form to &quot; so it displays correctly
+		/// </summary>
+		/// <param name="result">The XAmple XML result</param>
+		/// <returns>Corrected result</returns>
+		private static XDocument FixAnyDoubleQuotes(XDocument result)
+		{
+			var forms = result.Descendants("form");
+			var elem = forms.FirstOrDefault();
+			if (elem != null)
+			{
+				result = FixAnyDoubleQuotes(result, elem.Value);
+			}
+			return result;
+		}
+
+		public static XDocument FixAnyDoubleQuotes(XDocument result, string nodeValue)
+		{
+			if (nodeValue != null && nodeValue.Contains("&quot;"))
+			{
+				// The Contains method above compared to what is replaced below appears odd
+				// but what is really "&amp;quot;" is treated as "&quot;" in the node.
+				string fixedQuote = result.ToString().Replace("&amp;quot;", "&quot;");
+				XDocument fixedResult = XDocument.Parse(fixedQuote);
+				result = fixedResult;
+			}
+
+			return result;
 		}
 	}
 }
