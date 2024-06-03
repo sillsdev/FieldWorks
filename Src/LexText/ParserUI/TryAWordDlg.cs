ecef002cf3e9c9a7cefbfef2346baadfe18b7ef6
John Maxwell <maxwellparc@gmail.com>
2024-06-03 08:27:26 -0700

    Fix both quotes and angle brackets

--- Src/LexText/ParserUI/TryAWordDlg.cs
+++ Src/LexText/ParserUI/TryAWordDlg.cs
@@ -410,8 +410,9 @@ namespace SIL.FieldWorks.LexText.Controls
 					// Display a "processing" message (and include info on how to improve the results)
 					var uri = new Uri(Path.Combine(TransformPath, "WhileTracing.htm"));
 					m_htmlControl.URL = uri.AbsoluteUri;
+					sWord = new System.Xml.Linq.XText(sWord).ToString();  // LT-10373 XML special characters cause a crash; change it so HTML/XML works
+					sWord = sWord.Replace("\"", "&quot;");  // LT-10373 same for double quote
 					sWord = sWord.Replace(' ', '.'); // LT-7334 to allow for phrases; do this at the last minute
-					sWord = new System.Xml.Linq.XText(sWord).ToString();  // LT-10373 a double quote causes a crash; change it so HTML/XML works
 					m_parserListener.Connection.TryAWordDialogIsRunning = true; // make sure this is set properly
 					m_tryAWordResult = m_parserListener.Connection.BeginTryAWord(sWord, DoTrace, selectedTraceMorphs);
 					// waiting for result, so disable Try It button
