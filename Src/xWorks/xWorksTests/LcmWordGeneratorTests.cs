b17eaf3dcc791487608c48bab45fefbc0c6091ff
Mark Kidder <83427558+mark-sil@users.noreply.github.com>
2024-07-25 13:00:05 -0400

    Word Export: Generate unique display names (#110)
    Word Export: Generate unique display names
    
    - Generate unique display names when the FLEX style name is
      different, but the style display name is the same.
    - Generate the Sense number style in the same method that adds
      the sense number.
    - Move the code that adds the sense number before and after text
      to the same method that adds the sense number.

--- Src/xWorks/xWorksTests/LcmWordGeneratorTests.cs
+++ Src/xWorks/xWorksTests/LcmWordGeneratorTests.cs
@@ -72,6 +72,10 @@ namespace SIL.FieldWorks.XWorks
 				styles.Add(new BaseStyleInfo { Name = DictionaryNormal });
 			if (!styles.Contains("Dictionary-Headword"))
 				styles.Add(new BaseStyleInfo { Name = "Dictionary-Headword", IsParagraphStyle = false});
+			if (!styles.Contains("Abbreviation"))
+				styles.Add(new BaseStyleInfo { Name = "Abbreviation", IsParagraphStyle = false });
+			if (!styles.Contains("Dictionary-SenseNumber"))
+				styles.Add(new BaseStyleInfo { Name = "Dictionary-SenseNumber", IsParagraphStyle = false });
 
 			m_Clerk = CreateClerk();
 			m_propertyTable.SetProperty("ActiveClerk", m_Clerk, false);
@@ -212,6 +216,98 @@ namespace SIL.FieldWorks.XWorks
 				"/w:document/w:body/w:p/w:r/w:br[@w:type='textWrapping']",
 				2,
 				WordNamespaceManager);
+		}
+
+		[Test]
+		public void GenerateUniqueStyleName()
+		{
+			var wsOpts = ConfiguredXHTMLGeneratorTests.GetWsOptionsForLanguages(new[] { "en" });
+			var glossNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "Gloss",
+				DictionaryNodeOptions = wsOpts,
+				Style = "Dictionary-Headword"
+			};
+			var sensesNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "Senses",
+				DictionaryNodeOptions = ConfiguredXHTMLGeneratorTests.GetSenseNodeOptions(),
+				Children = new List<ConfigurableDictionaryNode> { glossNode },
+				Style = DictionaryNormal
+			};
+			var mainEntryNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "LexEntry",
+				Children = new List<ConfigurableDictionaryNode> { sensesNode },
+				Style = DictionaryNormal
+			};
+			CssGeneratorTests.PopulateFieldsForTesting(mainEntryNode);
+			var entry = ConfiguredXHTMLGeneratorTests.CreateInterestingLexEntry(Cache);
+
+			//SUT
+			var result = ConfiguredLcmGenerator.GenerateContentForEntry(entry, mainEntryNode, null, DefaultSettings, 0) as DocFragment;
+			glossNode.Style = "Abbreviation";
+			var result2 = ConfiguredLcmGenerator.GenerateContentForEntry(entry, mainEntryNode, null, DefaultSettings, 0) as DocFragment;
+
+			Assert.True(result.mainDocPart.RootElement.OuterXml.Contains("Gloss[lang='en']"));
+			Assert.True(result2.mainDocPart.RootElement.OuterXml.Contains("Gloss2[lang='en']"));
+		}
+
+		[Test]
+		public void GenerateSenseNumberData()
+		{
+			var wsOpts = ConfiguredXHTMLGeneratorTests.GetWsOptionsForLanguages(new[] { "en" });
+			var senseOptions = new DictionaryNodeSenseOptions
+			{
+				BeforeNumber = "BEF",
+				AfterNumber = "AFT",
+				NumberingStyle = "%d",
+				NumberEvenASingleSense = true
+			};
+			var glossNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "Gloss",
+				DictionaryNodeOptions = wsOpts,
+				Style = "Dictionary-Headword"
+			};
+			var subSenseNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "SensesOS",
+				CSSClassNameOverride = "senses",
+				DictionaryNodeOptions = senseOptions,
+				Children = new List<ConfigurableDictionaryNode> { glossNode },
+				Style = DictionaryNormal
+			};
+			var sensesNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "SensesOS",
+				CSSClassNameOverride = "senses",
+				DictionaryNodeOptions = senseOptions,
+				Children = new List<ConfigurableDictionaryNode> { glossNode, subSenseNode },
+				Style = DictionaryNormal
+			};
+
+			var mainEntryNode = new ConfigurableDictionaryNode
+			{
+				FieldDescription = "LexEntry",
+				Children = new List<ConfigurableDictionaryNode> { sensesNode },
+				Style = DictionaryNormal
+			};
+			CssGeneratorTests.PopulateFieldsForTesting(mainEntryNode);
+			var testEntry = ConfiguredXHTMLGeneratorTests.CreateInterestingLexEntry(Cache);
+			int wsEn = Cache.WritingSystemFactory.GetWsFromStr("en");
+			ConfiguredXHTMLGeneratorTests.AddSenseAndTwoSubsensesToEntry(testEntry, "second gloss", Cache, wsEn);
+
+			//SUT
+			var result = ConfiguredLcmGenerator.GenerateContentForEntry(testEntry, mainEntryNode, null, DefaultSettings, 0) as DocFragment;
+
+			// The four important pieces of data contained in the run:
+			// 1. Sense style:					Sense Number[lang='en']
+			// 2. Sense number before text:		BEF
+			// 3. Sense number:					2
+			// 4. Sense number after text:		AFT
+			const string senseNumberTwoRun = "<w:r><w:rPr><w:rStyle w:val=\"Sense Number[lang='en']\" /></w:rPr><w:t xml:space=\"preserve\">BEF</w:t><w:t xml:space=\"preserve\">2</w:t><w:t xml:space=\"preserve\">AFT</w:t></w:r>";
+			Assert.True(result.mainDocPart.RootElement.OuterXml.Contains(senseNumberTwoRun));
 		}
 	}
 }
