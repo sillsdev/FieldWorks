0b6cd5d0d1390d663a0f2432f7d0f279aca68461
John T Maxwell III <maxwellparc@gmail.com>
2024-08-05 18:02:24 -0700

    Fix 21855: Scrolling function in Parse Test Reports (#128)

--- Src/LexText/ParserUI/ParserReportsDialog.xaml.cs
+++ Src/LexText/ParserUI/ParserReportsDialog.xaml.cs
@@ -38,6 +38,17 @@ namespace SIL.FieldWorks.LexText.Controls
 			DataContext = new ParserReportsViewModel { ParserReports = parserReports };
 		}
 
+		private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			ScrollViewer scrollViewer = sender as ScrollViewer;
+			if (scrollViewer != null)
+				if (e.Delta > 0)
+					scrollViewer.LineUp();
+				else
+					scrollViewer.LineDown();
+			e.Handled = true;
+		}
+
 		public void ShowParserReport(object sender, RoutedEventArgs e)
 		{
 			foreach (var report in ParserReports)
