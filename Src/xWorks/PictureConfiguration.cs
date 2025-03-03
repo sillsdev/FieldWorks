ddb1d112e288ecf36384901c493b9d215c6f4ca7
Jason Naylor <jason_naylor@sil.org>
2025-03-03 15:44:18 -0800

    Add Pictures to DictionaryConfiguration options (LT-22053)

---
+++ Src/xWorks/PictureConfiguration.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Xml.Serialization;
+
+namespace SIL.FieldWorks.XWorks
+{
+	/// <summary>
+	/// Configuration shared by all pictures in a dictionary configuration.
+	/// </summary>
+	public class PictureConfiguration
+	{
+		public PictureConfiguration(PictureConfiguration other)
+		{
+			Alignment = other.Alignment;
+			Width = other.Width;
+		}
+
+		public PictureConfiguration() { }
+
+		[XmlAttribute("alignment")]
+		public AlignmentType Alignment { get; set; } = AlignmentType.Center;
+
+		/// <summary>
+		/// Width of the picture in inches.
+		/// </summary>
+		[XmlAttribute("width")]
+		public float Width { get; set; } = 1.0f;
+	}
+}
