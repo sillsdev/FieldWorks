1220e12ea066770631c404e7979ec757b113fce2
John Maxwell <maxwellparc@gmail.com>
2025-03-05 08:39:15 -0800

    Add unit tests for LT-22058, LT-22059, and LT-22060

---
+++ Src/LexText/Interlinear/ITextDllTests/XLingPaperExporterTests.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using NUnit.Framework;
+using SIL.LCModel.Utils;
+using SIL.FieldWorks.Common.FwUtils;
+using System.Text.RegularExpressions;
+
+
+namespace SIL.FieldWorks.IText
+{
+	internal class XLingPaperExporterTests
+	{
+		/// <summary>
+		/// Location of test files
+		/// </summary>
+		protected string m_sTestPath;
+		protected string m_sTransformPath;
+		protected XslCompiledTransform m_xmlTransform;
+		readonly Dictionary<string, XPathDocument> m_mapXmlDocs = new Dictionary<string, XPathDocument>();
+
+		[OneTimeSetUp]
+		public void FixtureSetup()
+		{
+			m_sTestPath = Path.Combine(FwDirectoryFinder.SourceDirectory, "LexText", "Interlinear", "ITextDllTests", "XLingPaperTransformerTestsDataFiles");
+			m_sTransformPath = Path.Combine(FwDirectoryFinder.FlexFolder, "Export Templates", "Interlinear", "xml2XLingPapConcatMorphemes.xsl");
+			m_xmlTransform = new XslCompiledTransform();
+			m_xmlTransform.Load(m_sTransformPath);
+		}
+
+		[Test]
+		public void TestGilaki01()
+		{
+			RunXmlTest("Gilaki01");
+		}
+
+		[Test]
+		public void TestHalbiBUD2()
+		{
+			RunXmlTest("HalbiBUD2");
+		}
+
+		[Test]
+		public void TestHalbiCS3()
+		{
+			RunXmlTest("HalbiCS3");
+		}
+
+		[Test]
+		public void TestHalbiST1()
+		{
+			RunXmlTest("HalbiST1");
+		}
+
+		[Test]
+		public void TestJibiyal2Texts()
+		{
+			RunXmlTest("Jibiyal2Texts");
+		}
+
+		[Test]
+		public void TestnszEnglishWords()
+		{
+			RunXmlTest("nszEnglishWords");
+		}
+
+		[Test]
+		public void TestSETCorn()
+		{
+			RunXmlTest("SETCorn");
+		}
+
+		[Test]
+		public void TestUrim2Kids()
+		{
+			RunXmlTest("Urim2Kids");
+		}
+
+		private void RunXmlTest(string testName)
+		{
+			string inputFileName = Path.Combine(m_sTestPath, "Phase1-" + testName + ".xml");
+			string goldFileName = Path.Combine(m_sTestPath, testName + "Old.xml");
+			ApplyTransform(inputFileName, m_xmlTransform, goldFileName);
+		}
+
+		private void ApplyTransform(string sInput, XslCompiledTransform transform, string sExpectedOutput)
+		{
+			XPathDocument inputXdoc = new XPathDocument(sInput);
+			string sOutput = FileUtils.GetTempFile("xml");
+			using (var result = new StreamWriter(sOutput))
+			{
+				transform.Transform(inputXdoc, null, result);
+				result.Close();
+			}
+			CheckOutputEquals(sExpectedOutput, sOutput);
+			// by deleting it here instead of a finally block, when it fails, we can see what the result is.
+			File.Delete(sOutput);
+		}
+
+		private void CheckOutputEquals(string sExpectedResultFile, string sActualResultFile)
+		{
+			string sExpected, sActual;
+			using (var expected = new StreamReader(sExpectedResultFile))
+				sExpected = expected.ReadToEnd();
+			using (var actual = new StreamReader(sActualResultFile))
+				sActual = actual.ReadToEnd();
+			// Eliminate spurious differences.
+			sActual = NormalizeXmlString(sActual);
+			sExpected = NormalizeXmlString(sExpected);
+			var sb = new StringBuilder();
+			sb.Append("Expected file was ");
+			sb.AppendLine(sExpectedResultFile);
+			sb.Append("Actual file was ");
+			sb.AppendLine(sActualResultFile);
+			Assert.AreEqual(sExpected, sActual, sb.ToString());
+		}
+
+		private string NormalizeXmlString(string xmlString)
+		{
+			xmlString = xmlString.Replace("\r", "");
+			xmlString = xmlString.Replace("\n", "");
+			xmlString = xmlString.Replace("UTF", "utf");
+			xmlString = Regex.Replace(xmlString, @"\s+<", "<");
+			xmlString = Regex.Replace(xmlString, @"\s+/>", "/>");
+			xmlString = xmlString.Replace("<gloss lang=\"en-lexGloss\"></gloss>", "<gloss lang=\"en-lexGloss\"/>");
+			xmlString = xmlString.Replace("<gloss lang=\"es-lexGloss\"></gloss>", "<gloss lang=\"es-lexGloss\"/>");
+			xmlString = xmlString.Replace("<gloss lang=\"en-wordGloss\"></gloss>", "<gloss lang=\"en-wordGloss\"/>");
+			xmlString = xmlString.Replace("<langData lang=\"stp-x-stp-morpheme\"></langData>", "<langData lang=\"stp-x-stp-morpheme\"/>");
+			xmlString = xmlString.Replace("<object type=\"tVariantTypes\"></object>", "<object type=\"tVariantTypes\"/>");
+			xmlString = xmlString.Replace("<shortTitle></shortTitle>", "<shortTitle/>");
+			return xmlString;
+		}
+	}
+}
