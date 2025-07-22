43f6e34baa83a417335982648a541563428f1422
Andy Black <blackhandrew@gmail.com>
2025-07-22 10:52:59 -0700

    LT-22165 HC does not handle multiple compounds in a row (#394)
    * Add new HC parameters for compound rules
    
    Change-Id: I7970523b22e368f39dacd7caf54b08e01ff0c9f6
    
    * Pass in compound rules to show in new dialog
    
    Change-Id: If2260d196dbb92d132fb770864901fb5c148ea88
    
    * LT-22165 Add HC Max Compound Rules dialog
    
    Change-Id: Ia7cd5d510af61362aa6d3766632b3549967a6673
    
    * Fix null crash
    
    Change-Id: Ib7a3f3539de89c96bf4b23e3722f8e5aa1e3dd23
    
    * Add comments explaining MaxRoots values for Hermit Crab
    
    Change-Id: Idba35759cc5a6de32a13955f292956b324036cc1
    
    * Allow HC MaxRoots to be 1
    
    Change-Id: Ic6892746575b5fe3e79fc6b19da5dff97912bca9
    
    * LT-22165 Make HC MaxRoots be whatever the dialog allows
    
    Change-Id: I185b115f7f45a588f7a22e2740401dd9aced91fd

---
+++ Src/LexText/ParserUI/HCMaxCompoundRulesDlg.Designer.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using XCore;
+
+namespace SIL.FieldWorks.LexText.Controls
+{
+	partial class HCMaxCompoundRulesDlg
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose(bool disposing)
+		{
+			Debug.WriteLineIf(!disposing, "****** Missing Dispose() call for " + GetType().Name + ". ****** ");
+			// Must not be run more than once.
+			if (IsDisposed)
+				return;
+
+			if (disposing)
+			{
+				if (m_dsCompoundRules != null)
+				{
+					m_dsCompoundRules.Dispose();
+					m_dsCompoundRules = null;
+				}
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HCMaxCompoundRulesDlg));
+			this.m_dataGrid = new System.Windows.Forms.DataGrid();
+			this.m_btnCancel = new System.Windows.Forms.Button();
+			this.m_btnOK = new System.Windows.Forms.Button();
+			this.m_btnHelp = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.m_dataGrid)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// m_dataGrid
+			// 
+			this.m_dataGrid.CaptionText = "Set maximum applications for compound rules";
+			this.m_dataGrid.DataMember = "";
+			this.m_dataGrid.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+			this.m_dataGrid.Location = new System.Drawing.Point(0, 0);
+			this.m_dataGrid.Name = "m_dataGrid";
+			this.m_dataGrid.Size = new System.Drawing.Size(800, 379);
+			this.m_dataGrid.TabIndex = 4;
+			// 
+			// m_btnCancel
+			// 
+			this.m_btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.m_btnCancel.Location = new System.Drawing.Point(368, 400);
+			this.m_btnCancel.Name = "m_btnCancel";
+			this.m_btnCancel.Size = new System.Drawing.Size(112, 35);
+			this.m_btnCancel.TabIndex = 1;
+			this.m_btnCancel.Text = "Cancel";
+			// 
+			// m_btnOK
+			// 
+			this.m_btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.m_btnOK.Location = new System.Drawing.Point(247, 400);
+			this.m_btnOK.Name = "m_btnOK";
+			this.m_btnOK.Size = new System.Drawing.Size(112, 35);
+			this.m_btnOK.TabIndex = 2;
+			this.m_btnOK.Text = "OK";
+			this.m_btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// m_btnHelp
+			// 
+			this.m_btnHelp.Location = new System.Drawing.Point(489, 400);
+			this.m_btnHelp.Name = "m_btnHelp";
+			this.m_btnHelp.Size = new System.Drawing.Size(112, 35);
+			this.m_btnHelp.TabIndex = 3;
+			this.m_btnHelp.Text = "Help";
+			this.m_btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+			// 
+			// HCMaxCompoundRulesDlg
+			// 
+			this.AcceptButton = this.m_btnOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.m_btnCancel;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.m_btnOK);
+			this.Controls.Add(this.m_btnCancel);
+			this.Controls.Add(this.m_btnHelp);
+			this.Controls.Add(this.m_dataGrid);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "HCMaxCompoundRulesDlg";
+			this.Text = "HC Max Compound Rules Applications";
+			((System.ComponentModel.ISupportInitialize)(this.m_dataGrid)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button m_btnCancel;
+		private System.Windows.Forms.Button m_btnOK;
+		private Button m_btnHelp;
+	}
+}
