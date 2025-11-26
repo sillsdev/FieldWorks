e80fcd96f32610849d249aab9a95d9bbf2f46bc3
Hasso <hasso_pape@sil.org>
2025-11-26 11:34:33 -0600

    Clean up and modernise
    Change-Id: I628fc99c77ea267cfcef4fda877c5c584b0c88ae

--- Src/Common/Controls/XMLViews/XMLViewsTests/XmlBrowseViewBaseVcTests.cs
+++ Src/Common/Controls/XMLViews/XMLViewsTests/XmlBrowseViewBaseVcTests.cs
@@ -178,35 +178,6 @@ namespace XMLViewsTests
 			CollectionAssert.AreEqual(new List<string> { "Ref", "Occurrence" }, columnLabels);
 		}
 
-		/// <remarks>TODO (Hasso) 2025.11: This test needs further setup to find the layout XmlNode in the LayoutCache</remarks>
-		public void IsValidColumnSpec_HasLayout_TODO()
-		{
-			var vc = new XmlBrowseViewBaseVc { PossibleColumnSpecs = new List<XmlNode>(), ListItemsClass = -1 /* can't be 0 */ };
-			var possibleColumns = new XmlDocument();
-			possibleColumns.LoadXml(@"<columns>
-	<column label='Name' width='13%' layout='Name' ws='$ws=best analysis' field='Name'/>
-	<column label='Abbreviation' width='10%' layout='Abbreviation' ws='$ws=best analysis' field='Abbreviation'/>
-	<column label='Values' width='24%' multipara='true' layout='TypeOrValues' visibility='menu'/>
-</columns>");
-			foreach (XmlNode node in possibleColumns.DocumentElement.GetElementsByTagName("column"))
-			{
-				vc.PossibleColumnSpecs.Add(node);
-			}
-
-			var validColumns = new XmlDocument();
-			validColumns.LoadXml(@"<root version='18'>
-	<column label='Name' width='13%' layout='Name' ws='$ws=best analysis' field='Name'/>
-	<column width='10%' layout='Abbreviation' ws='$ws=en' field='Abbreviation' originalWs='best analysis' originalLabel='Abbreviation' label='Abreviatura (Eng)'/>
-	<column width='10%' layout='Abbreviation' ws='$ws=es' field='Abbreviation' originalWs='best analysis' originalLabel='Abbreviation' label='Abreviatura (Spa)'/>
-</root>");
-
-			// SUT
-			foreach (XmlNode node in validColumns.DocumentElement.GetElementsByTagName("column"))
-			{
-				Assert.IsTrue(vc.IsValidColumnSpec(node), $"Should have found this node to be valid: {node.OuterXml}");
-			}
-		}
-
 		/// <summary>
 		/// Tests that IsValidColumnSpec can identify valid columns based on their labels or originalLabels.
 		/// Artificially skips the layout check because that requires a more complex setup.
