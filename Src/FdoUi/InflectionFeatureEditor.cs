077094774bfbd046e2946758f25668efa0cdf77c
John Maxwell <maxwellparc@gmail.com>
2025-12-03 10:29:02 -0800

    Fix LT-22326: Crash in Lexicon . Bulk Edit Entries view

--- Src/FdoUi/InflectionFeatureEditor.cs
+++ Src/FdoUi/InflectionFeatureEditor.cs
@@ -390,7 +390,7 @@ namespace SIL.FieldWorks.FdoUi
 						InitMsa(msmTarget, msm.PartOfSpeechRA.Hvo);
 					}
 				}
-				if (msmTarget == null)
+				if (msmTarget == null && pos != null)
 				{
 					// Nothing we can reuse...make a new one.
 					msmTarget = m_cache.ServiceLocator.GetInstance<IMoStemMsaFactory>().Create();
