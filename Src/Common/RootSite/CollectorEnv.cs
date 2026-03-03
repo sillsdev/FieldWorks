19a54468bc063e3a0cec722800f07250d917ba98
John Maxwell <maxwellparc@gmail.com>
2026-03-03 11:31:39 -0800

    Added close to param

--- Src/Common/RootSite/CollectorEnv.cs
+++ Src/Common/RootSite/CollectorEnv.cs
@@ -2902,7 +2902,7 @@ namespace SIL.FieldWorks.Common.RootSites
 		/// <param name="baseEnv">The base env.</param>
 		/// <param name="sda">Date access to get prop values etc.</param>
 		/// <param name="hvoRoot">The root object to display, if m_baseEnv is null.
-		/// <param name="addEOL">Add an EOL between lines.
+		/// <param name="addEOL">Add an EOL between lines.</param>
 		/// If baseEnv is not null, hvoRoot is ignored.</param>
 		/// ------------------------------------------------------------------------------------
 		public TsStringCollectorEnv(IVwEnv baseEnv, ISilDataAccess sda, int hvoRoot, bool addEOL = false) :
