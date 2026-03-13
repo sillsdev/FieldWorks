e919f5dfd1a2a22d26df0dc77fa6982e693b4a98
Ariel Ror. <ariel_rorabaugh@sil.org>
2026-03-12 20:43:31 -0400

    Update No Media message (#758)
    Keep "No Media" message when no media filepath is specified.
    
    Add the message "Media not found: check Info tab for file name"
    for the case when a media filepath is specified but the file
    does not exist.

--- Src/LexText/Interlinear/ITextStrings.Designer.cs
+++ Src/LexText/Interlinear/ITextStrings.Designer.cs
@@ -1406,6 +1406,15 @@ namespace SIL.FieldWorks.IText {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Media not found: check Info tab for file name.
+        /// </summary>
+        internal static string ksMediaNotFound {
+            get {
+                return ResourceManager.GetString("ksMediaNotFound", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to This utility finds groups of analyses that have the same word category and morphological analysis. It merges such groups into a single analysis. It keeps all the glosses, except that if some glosses are duplicates (in all writing systems) such groups will also be merged. Analyzed texts which use any of the merged analyses will be made to use the merged one..
         /// </summary>
         internal static string ksMergeAnalysesAttemptsTo {
