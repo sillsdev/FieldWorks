sillsdev/FieldWorks

commit fd245f0c5fcd26bcbb54d3e482f420c0fdc64b49

LT-21615: Fix delete functionality not working for Sem Doms (#954)
Update Editable property in XWorksViewBase when switching back
to an already existing record clerk.

Src/xWorks/XWorksViewBase.cs
@@ -170,15 +170,27 @@ namespace SIL.FieldWorks.XWorks
 			get
 			{
 				if (m_clerk != null)
+				{
+					UpdateClerkEditable(m_clerk);
 					return m_clerk;
+				}
 				if (m_mediator == null)
 					return null; // Avoids a null reference exception, if there is no mediator at all.
 				if (m_propertyTable == null)
 					return null; // Avoids a null reference exception, if there is no property table at all.
+
 				m_haveActiveClerk = false;
 				m_clerk = RecordClerk.FindClerk(m_propertyTable, m_vectorName);
-				if (m_clerk != null && m_clerk.IsActiveInGui)
-					m_haveActiveClerk = true;
+				if (m_clerk != null)
+				{
+					UpdateClerkEditable(m_clerk);
+
+					if (m_clerk.IsActiveInGui)
+					{
+						m_haveActiveClerk = true;
+					}
+				}
+
 				return m_clerk;
 			}
 		}
@@ -192,14 +204,25 @@ namespace SIL.FieldWorks.XWorks
 				loadList,
 				true
 			);
-			clerk.Editable = XmlUtils.GetOptionalBooleanAttributeValue(
-				m_configurationParameters,
-				"allowInsertDeleteRecord",
-				true
-			);
+			UpdateClerkEditable(clerk);
 			return clerk;
 		}
 
+		/// <summary>
+		/// Updates the Editable property of the given clerk, based on the "allowInsertDeleteRecord" attribute in the configuration parameters.
+		/// </summary>
+		/// <param name="clerk"></param>
+		internal void UpdateClerkEditable(RecordClerk clerk)
+		{
+			if (clerk != null)
+			{
+				clerk.Editable = XmlUtils.GetOptionalBooleanAttributeValue(
+					m_configurationParameters,
+					"allowInsertDeleteRecord",
+					true
+				);
+			}
+		}
 		/// <summary>
 		/// Get/Set the Clerk used by the view.
 		/// </summary>

commit 86e7c417e2b1382d0bf862935b2503aa280595ca

LT-22572: Add IsDisposed check
This change also fixes these defects:
LT-22573
LT-22574

The regression was introduced with Commit fd245f0c.

Src/xWorks/XWorksViewBase.cs
@@ -208,7 +208,7 @@ namespace SIL.FieldWorks.XWorks
 		/// <param name="clerk"></param>
 		internal void UpdateClerkEditable(RecordClerk clerk)
 		{
-			if (clerk != null)
+			if (clerk?.IsDisposed == false)
 			{
 				clerk.Editable = XmlUtils.GetOptionalBooleanAttributeValue(
 					m_configurationParameters,