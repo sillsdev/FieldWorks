952997d5c443ee12c34fe5a020513e3fcddd9ecd
John Lambert <john_lambert@sil.org>
2026-08-26 17:24:47 -0400

    test: characterize Inventory view snapshots

---
+++ Src/xWorks/Avalonia/Composer/InventoryViewDefinitionSource.cs
@@ -0,0 +1,86 @@
+// Copyright (c) 2026 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using System;
+using System.Collections.Generic;
+using SIL.FieldWorks.Common.FwAvalonia.ViewDefinition;
+using SIL.LCModel.Core.KernelInterfaces;
+using XCore;
+
+namespace SIL.FieldWorks.XWorks
+{
+	/// <summary>
+	/// Creates immutable view-definition snapshots from an effective project layout inventory.
+	/// </summary>
+	public sealed class InventoryViewDefinitionSource
+	{
+		private readonly Inventory _layouts;
+		private readonly string _partsXml;
+		private readonly IFwMetaDataCache _metadataCache;
+
+		/// <summary>
+		/// Creates a source backed by the current effective layouts and immutable merged parts
+		/// XML.
+		/// </summary>
+		public InventoryViewDefinitionSource(Inventory layouts, string partsXml,
+			IFwMetaDataCache metadataCache)
+		{
+			_layouts = layouts ?? throw new ArgumentNullException(nameof(layouts));
+			_partsXml = partsXml ?? throw new ArgumentNullException(nameof(partsXml));
+			_metadataCache = metadataCache ?? throw new ArgumentNullException(nameof(metadataCache));
+		}
+
+		/// <summary>
+		/// Gets the effective detail layout snapshot, or null when the class hierarchy has no
+		/// match.
+		/// </summary>
+		public ViewDefinitionSourceSnapshot GetSnapshot(string className, string layoutName,
+			string choiceGuid = null)
+		{
+			var classId = _metadataCache.GetClassId(className);
+			var baseClassMap = new Dictionary<string, string>(StringComparer.Ordinal);
+			string resolvedClassName;
+			string layoutXml;
+
+			while (true)
+			{
+				resolvedClassName = _metadataCache.GetClassName(classId);
+				var layout = _layouts.GetElement("layout",
+					new[] { resolvedClassName, "detail", layoutName, choiceGuid });
+				if (layout == null)
+				{
+					layout = _layouts.GetElement("layout",
+						new[] { resolvedClassName, "detail", layoutName, null });
+				}
+
+				if (layout != null)
+				{
+					layoutXml = layout.OuterXml;
+					break;
+				}
+
+				if (classId == 0)
+					return null;
+				var baseId = _metadataCache.GetBaseClsId(classId);
+				if (baseId == classId)
+					return null;
+				baseClassMap[resolvedClassName] = _metadataCache.GetClassName(baseId);
+				classId = baseId;
+			}
+
+			var chain = classId;
+			while (chain != 0)
+			{
+				var baseId = _metadataCache.GetBaseClsId(chain);
+				if (baseId == chain || baseId == 0)
+					break;
+				baseClassMap[_metadataCache.GetClassName(chain)] = _metadataCache.GetClassName(baseId);
+				chain = baseId;
+			}
+
+			return new ViewDefinitionSourceSnapshot(resolvedClassName, "detail", layoutXml,
+				_partsXml, baseClassMap);
+		}
+	}
+}
---
+++ Src/xWorks/xWorksTests/Avalonia/Composer/InventoryViewDefinitionSourceTests.cs
@@ -0,0 +1,163 @@
+// Copyright (c) 2026 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using NUnit.Framework;
+using SIL.LCModel;
+using XCore;
+
+namespace SIL.FieldWorks.XWorks
+{
+	[TestFixture]
+	public class InventoryViewDefinitionSourceTests : MemoryOnlyBackendProviderRestoredForEachTestTestBase
+	{
+		private const string PartsXml = @"
+<PartInventory><bin>
+  <part id='LexEntry-Detail-CitationForm'>
+    <slice label='CitationForm' editor='multistring' field='CitationForm'/>
+  </part>
+</bin></PartInventory>";
+
+		private string _projectPath;
+
+		public override void TestSetup()
+		{
+			base.TestSetup();
+			_projectPath = Path.Combine(Path.GetTempPath(), "fw-inventory-source-" + Guid.NewGuid().ToString("N"));
+			Directory.CreateDirectory(_projectPath);
+		}
+
+		public override void TestTearDown()
+		{
+			base.TestTearDown();
+			if (Directory.Exists(_projectPath))
+				Directory.Delete(_projectPath, true);
+		}
+
+		[Test]
+		public void GetSnapshot_ReturnsShippedLayoutAndMergedParts()
+		{
+			const string layoutXml = @"
+<LayoutInventory>
+  <layout class='LexEntry' type='detail' name='Normal'>
+    <part ref='CitationForm'/>
+  </layout>
+</LayoutInventory>";
+			var layouts = CreateLayoutInventory(layoutXml);
+			var parts = CreatePartsInventory();
+			var source = CreateSource(layouts, parts);
+
+			var snapshot = source.GetSnapshot("LexEntry", "Normal");
+
+			Assert.That(snapshot, Is.Not.Null);
+			Assert.That(XElement.Parse(snapshot.LayoutXml).Element("part")?.Attribute("ref")?.Value,
+				Is.EqualTo("CitationForm"));
+			Assert.That(XElement.Parse(snapshot.PartsXml).Descendants("part").Single().Attribute("id")?.Value,
+				Is.EqualTo("LexEntry-Detail-CitationForm"));
+		}
+
+		[Test]
+		public void GetSnapshot_AfterPersistedOverrideReturnsNewXmlWithoutChangingPriorSnapshot()
+		{
+			const string layoutXml = @"
+<LayoutInventory>
+  <layout class='LexEntry' type='detail' name='Normal'>
+    <part ref='CitationForm' visibility='always'/>
+  </layout>
+</LayoutInventory>";
+			var layouts = CreateLayoutInventory(layoutXml);
+			var source = CreateSource(layouts, CreatePartsInventory());
+			var first = source.GetSnapshot("LexEntry", "Normal");
+			var changed = new XmlDocument();
+			changed.LoadXml(@"<layout class='LexEntry' type='detail' name='Normal'>
+  <part ref='CitationForm' visibility='never'/>
+</layout>");
+
+			layouts.PersistOverrideElement(changed.DocumentElement);
+			var second = source.GetSnapshot("LexEntry", "Normal");
+
+			Assert.That(GetVisibility(first), Is.EqualTo("always"));
+			Assert.That(GetVisibility(second), Is.EqualTo("never"));
+		}
+
+		[Test]
+		public void GetSnapshot_ChoiceGuidUsesExactLayoutThenFallsBackToLayoutWithoutChoiceGuid()
+		{
+			const string selectedGuid = "11111111-1111-1111-1111-111111111111";
+			const string unknownGuid = "22222222-2222-2222-2222-222222222222";
+			const string layoutXml = @"
+<LayoutInventory>
+  <layout class='LexEntry' type='detail' name='Normal' marker='fallback'/>
+  <layout class='LexEntry' type='detail' name='Normal'
+          choiceGuid='11111111-1111-1111-1111-111111111111' marker='exact'/>
+</LayoutInventory>";
+			var source = CreateSource(CreateLayoutInventory(layoutXml), CreatePartsInventory());
+
+			var exact = source.GetSnapshot("LexEntry", "Normal", selectedGuid);
+			var fallback = source.GetSnapshot("LexEntry", "Normal", unknownGuid);
+
+			Assert.That(XElement.Parse(exact.LayoutXml).Attribute("marker")?.Value, Is.EqualTo("exact"));
+			Assert.That(XElement.Parse(fallback.LayoutXml).Attribute("marker")?.Value, Is.EqualTo("fallback"));
+		}
+
+		[Test]
+		public void GetSnapshot_MissingDerivedLayoutUsesBaseLayoutAndRecordsPartResolutionMap()
+		{
+			const string layoutXml = @"
+<LayoutInventory>
+  <layout class='MoForm' type='detail' name='Normal'/>
+</LayoutInventory>";
+			var source = CreateSource(CreateLayoutInventory(layoutXml), CreatePartsInventory());
+
+			var snapshot = source.GetSnapshot("MoStemAllomorph", "Normal");
+
+			Assert.That(snapshot, Is.Not.Null);
+			Assert.That(snapshot.ClassName, Is.EqualTo("MoForm"));
+			Assert.That(snapshot.BaseClassMap, Is.EquivalentTo(new Dictionary<string, string>
+			{
+				["MoStemAllomorph"] = "MoForm"
+			}));
+		}
+
+		private InventoryViewDefinitionSource CreateSource(Inventory layouts, Inventory parts)
+		{
+			return new InventoryViewDefinitionSource(layouts, parts.Root.OuterXml,
+				Cache.MetaDataCacheAccessor);
+		}
+
+		private Inventory CreateLayoutInventory(string xml)
+		{
+			var keyAttributes = new Dictionary<string, string[]>
+			{
+				["layout"] = new[] { "class", "type", "name", "choiceGuid" }
+			};
+			var inventory = new Inventory("*.fwlayout", "/LayoutInventory/*", keyAttributes,
+				"InventoryViewDefinitionSourceTests", _projectPath);
+			inventory.LoadElements(xml, 0);
+			return inventory;
+		}
+
+		private static Inventory CreatePartsInventory()
+		{
+			var keyAttributes = new Dictionary<string, string[]>
+			{
+				["part"] = new[] { "id" }
+			};
+			var inventory = new Inventory("*Parts.xml", "/PartInventory/bin/*", keyAttributes,
+				"InventoryViewDefinitionSourceTests", "unused");
+			inventory.LoadElements(PartsXml, 0);
+			return inventory;
+		}
+
+		private static string GetVisibility(Common.FwAvalonia.ViewDefinition.ViewDefinitionSourceSnapshot snapshot)
+		{
+			return XElement.Parse(snapshot.LayoutXml).Element("part")?.Attribute("visibility")?.Value;
+		}
+	}
+}
